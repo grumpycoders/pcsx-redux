b227c321c13649946e48601ad78d917c2367306f
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2019-04-23 20:09:01 -0700

    Linux compilation fix.

--- src/core/debug.cc
+++ src/core/debug.cc
@@ -17,6 +17,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.           *
  ***************************************************************************/
 
+#include <iomanip>
 #include <sstream>
 
 #include "core/debug.h"
