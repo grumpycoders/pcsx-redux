33913efc7f8c45a2d3728720cd51acf5d9b2a958
Emmanuel Peralta <manz@ringum.net>
2019-06-13 22:24:18 +0200

    Fixes #if for macOS shader version.

--- src/gui/gui.h
+++ src/gui/gui.h
@@ -37,7 +37,7 @@
 #include "gui/widgets/vram-viewer.h"
 #include "main/settings.h"
 
-#if defined(__MACOSX__)
+#if defined(__APPLE__)
 #define GL_SHADER_VERSION "#version 410\n"
 #else
 #define GL_SHADER_VERSION "#version 300 es\n"
