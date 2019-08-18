8c007fc5cf9c2bb4d5379c30ca70dab042111783
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2019-08-17 21:59:02 -0700

    Preventing distcc to try and cross compile.

--- distcc-make.sh
+++ distcc-make.sh
@@ -2,6 +2,7 @@
 
 # This is for my own build environment, so it won't be much useful for anyone else.
 
+export DISTCC_NO_REWRITE_CROSS=1
 export DISTCC_HOSTS=10.12.1.2/60,lzo
 export CC="distcc clang-9"
 export CXX="distcc clang++-9"
