6a4d1a3cf4861a49a343c458aba134750f7b69a4
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2019-08-29 23:20:29 -0700

    Refining the process a bit.

--- src/mips/openbios/boot/boot.s
+++ src/mips/openbios/boot/boot.s
@@ -1,7 +1,35 @@
+.include "hwregs.inc"
+
     .section .boot,"ax",@progbits
     .align 2
     .global _reset
     .type _reset, @function
+
 _reset:
+    // set bios memory bus width and speed
     li    $t0, (0x13 << 16) | 0x243f
-    sw    $t0, 0x1f801010
+    sw    $t0, SBUS_DEV2_CTRL
+
+    // this may be here to let the hardware pick up the new bus settings
+    // before moving on with the actual code.
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
