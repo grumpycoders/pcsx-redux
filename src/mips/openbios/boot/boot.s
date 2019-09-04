94508a0218dbef7f90da075c29c1a1c8c4f494ce
Nicolas Pixel Noble <pixel@nobis-crew.org>
2019-09-03 20:26:49 -0700

    Factoring cache flush code.

--- src/mips/openbios/boot/boot.s
+++ src/mips/openbios/boot/boot.s
@@ -21,6 +21,7 @@
 
     .section .boot, "ax", @progbits
     .align 2
+    .global flushCache
     .global _reset
     .type _reset, @function
 
@@ -126,95 +127,7 @@ _boot:
     move  $30, $0
     move  $31, $0
 
-    // initializing cache
-    li    $t0, 0x804
-    sw    $t0, CACHE_CTRL
-
-    li    $t1, 0x10000
-    mtc0  $t1, $12
-    nop
-    nop
-
-    move  $t0, $0
-    li    $t2, 0x1000
-
-cache_init_1:
-    sw    $0, 0x00($t0)
-    sw    $0, 0x10($t0)
-    sw    $0, 0x20($t0)
-    sw    $0, 0x30($t0)
-    sw    $0, 0x40($t0)
-    sw    $0, 0x50($t0)
-    sw    $0, 0x60($t0)
-    sw    $0, 0x70($t0)
-    addi  $t0, 0x80
-    bne   $t0, $t2, cache_init_1
-
-    mtc0  $0, $12
-    nop
-
-    li    $t0, 0x800
-    sw    $t0, CACHE_CTRL
-
-    mtc0  $t1, $12
-    nop
-    nop
-
-    move  $t0, $0
-    li    $t2, 0x1000
-
-cache_init_2:
-    sw    $0, 0x00($t0)
-    sw    $0, 0x04($t0)
-    sw    $0, 0x08($t0)
-    sw    $0, 0x0c($t0)
-    sw    $0, 0x10($t0)
-    sw    $0, 0x14($t0)
-    sw    $0, 0x18($t0)
-    sw    $0, 0x1c($t0)
-    sw    $0, 0x20($t0)
-    sw    $0, 0x24($t0)
-    sw    $0, 0x28($t0)
-    sw    $0, 0x2c($t0)
-    sw    $0, 0x30($t0)
-    sw    $0, 0x34($t0)
-    sw    $0, 0x38($t0)
-    sw    $0, 0x3c($t0)
-    sw    $0, 0x40($t0)
-    sw    $0, 0x44($t0)
-    sw    $0, 0x48($t0)
-    sw    $0, 0x4c($t0)
-    sw    $0, 0x50($t0)
-    sw    $0, 0x54($t0)
-    sw    $0, 0x58($t0)
-    sw    $0, 0x5c($t0)
-    sw    $0, 0x60($t0)
-    sw    $0, 0x64($t0)
-    sw    $0, 0x68($t0)
-    sw    $0, 0x6c($t0)
-    sw    $0, 0x70($t0)
-    sw    $0, 0x74($t0)
-    sw    $0, 0x78($t0)
-    sw    $0, 0x7c($t0)
-    addi  $t0, 0x80
-    bne   $t0, $t2, cache_init_2
-
-    mtc0  $0, $12
-    nop
-
-    li    $t0, 0xa0000000
-    lw    $t1, 0($t0)
-    lw    $t1, 0($t0)
-    lw    $t1, 0($t0)
-    lw    $t1, 0($t0)
-    lw    $t1, 0($t0)
-    lw    $t1, 0($t0)
-    lw    $t1, 0($t0)
-    lw    $t1, 0($t0)
-    nop
-
-    li    $t0, 0x1e988
-    sw    $t0, CACHE_CTRL
+    jal  flushCache
 
     // ensuring cop0 is fully reset
     mtc0  $0, $7
