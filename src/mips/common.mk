6ef8eee250a75ee8ca749643cd05b17a14214634
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2019-09-30 22:22:54 +0200

    Fixing build.

--- src/mips/common.mk
+++ src/mips/common.mk
@@ -3,17 +3,22 @@ PREFIX = mipsel-linux-gnu
 CC = $(PREFIX)-gcc
 AR = $(PREFIX)-ar
 
+## If "TLOAD_ADDR" was specified in the Makefile, pass it to the linker.
+ifneq ($(strip $(TLOAD_ADDR)),)
+LDFLAGS += -Wl,--defsym,TLOAD_ADDR=$(TLOAD_ADDR)
+endif
+
 TARGETBASE = $(basename $(TARGET))
 
 ARCHFLAGS = -march=mips1 -mabi=32 -EL -msoft-float -Wa,-msoft-float -fno-pic -mno-shared -mno-abicalls
-ARCHFLAGS += -mno-gpopt -fomit-frame-pointer -nostartfiles -nostdinc -fno-builtin -fno-pic
+ARCHFLAGS += -mno-gpopt -fomit-frame-pointer -nostartfiles -nostdinc -fno-builtin -fno-pic -fno-stack-protector
 
 CPPFLAGS += -fno-builtin
 CPPFLAGS += $(ARCHFLAGS)
 CPPFLAGS += -I..
 
 LDFLAGS += -Wl,-Map=$(TARGETBASE).map -nostdlib -T$(LDSCRIPT) -static -Wl,--gc-sections
-LDFLAGS += $(ARCHFLAGS)
+LDFLAGS += $(ARCHFLAGS) -L../ps1sdk
 
 LDFLAGS += -g -O3 -flto
 CPPFLAGS += -g -O3 -flto
@@ -21,6 +26,9 @@ CPPFLAGS += -g -O3 -flto
 OBJS += $(addsuffix .o, $(basename $(SRCS)))
 
 all: $(TARGET)
+
+../ps1sdk/libps1sdk.a:
+	$(MAKE) -C ../ps1sdk all
 
 clean:
 	rm -f $(OBJS) $(TARGETBASE).psx $(TARGETBASE).elf $(TARGET).map $(TARGETBASE).bin $(TARGET)
@@ -31,11 +39,11 @@ $(TARGETBASE).bin: $(TARGETBASE).elf
 $(TARGETBASE).psx: $(TARGETBASE).elf
 	$(PREFIX)-objcopy -O binary $< $@
 
-$(TARGETBASE).a : $(OBJS)
-	$(AR) cru $(TARGET) $(OBJS)
+$(TARGETBASE).a: $(OBJS)
+	$(AR) cru $@ $(OBJS)
 
-$(TARGETBASE).elf: $(OBJS)
-	$(CC) $(LDFLAGS) -g -o $(TARGETBASE).elf $(OBJS)
+$(TARGETBASE).elf: $(OBJS) $(LIBS)
+	$(CC) $(LDFLAGS) -g -o $(TARGETBASE).elf $(OBJS) $(LIBS)
 
 %.o: %.S
 	$(CC) $(CPPFLAGS) -I.. -g -c -o $@ $<
