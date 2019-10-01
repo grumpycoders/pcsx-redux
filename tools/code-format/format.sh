6b47f79b843fe2ec34d3796ea88516c4af193f28
Aric S Marheine <5118895+asmblur@users.noreply.github.com>
2019-09-30 23:15:26 -0500

    inital support for switching between clang-format files.

--- tools/code-format/format.sh
+++ tools/code-format/format.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
+set -e
+
+if [ $# -gt 0 ]; then
+    CLANGF_SRC=`realpath -e "$1"`
+    if [ ! -f "$1" ]; then
+        echo "$1: no such file or directory" >&2
+        exit 1
+    fi
+fi
 
 cd $(dirname $0)/../..
+
+CLANGF_DEST=src/.clang-format
+
+rm -f "$CLANGF_DEST"
+
+if [ -z "$CLANGF_SRC" ]; then
+    CLANGF_SRC="src/.clang-format-commit"
+fi
+
+cp "$CLANGF_SRC" "$CLANGF_DEST"
+
+echo "Using \"$CLANGF_SRC\"..."
 
 docker build -t code-format tools/code-format
 docker run --rm=true -v "$(pwd)":/repository --user "$(id -u):$(id -g)" -t code-format
