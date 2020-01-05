20d51c36b37288a8e32b82aed1a8a3ef3e9bb4ca
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2020-01-05 11:15:12 -0800

    Adding libusb-posix skeleton.

---
+++ src/ftdi/abstract-libusb-posix.cc
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *   Copyright (C) 2019 PCSX-Redux authors                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.           *
+ ***************************************************************************/
+
+#ifndef _WIN32
+
+#include "ftdi/abstract.h"
+
+static std::vector<PCSX::FTDI::Device> s_devices;
+
+void PCSX::FTDI::DeviceList::scan() { abort(); }
+
+const std::vector<PCSX::FTDI::Device>& PCSX::FTDI::DeviceList::get() { return s_devices; }
+
+#endif
