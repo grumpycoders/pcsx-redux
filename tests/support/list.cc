b63b2b77d9d0bc60a61f5a6b40084c53b262b8d7
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2020-02-12 13:44:17 -0800

    Basic test framework.

---
+++ tests/support/list.cc
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *   Copyright (C) 2020 PCSX-Redux authors                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.           *
+ ***************************************************************************/
+
+#include "gtest/gtest.h"
+#include "support/list.h"
+
+class Element;
+typedef PCSX::Intrusive::List<Element> ListType;
+struct Element : public ListType::Node {
+    Element(int tag = 0) : m_tag(tag) {}
+    int m_tag = 0;
+};
+
+TEST(BasicList, EmptyList) {
+    ListType list;
+    EXPECT_TRUE(list.empty());
+}
