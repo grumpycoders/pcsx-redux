e360ef2577e4be93f46fe3e556303c80be79f45a
Nicolas Pixel Noble <pixel@nobis-crew.org>
2020-04-15 21:19:55 -0700

    Better syscall definition.

--- src/mips/common/syscalls/syscalls.h
+++ src/mips/common/syscalls/syscalls.h
@@ -22,18 +22,9 @@
 #include <stdarg.h>
 
 static __inline__ int syscall_strcmp(const char * s1, const char * s2) {
-    register const char * arg1 asm("a0") = s1;
-    register const char * arg2 asm("a1") = s2;
-    register int ret asm("v0");
-    __asm__ volatile(
-        "li $t2, 0xa0\n"
-        "li $t1, 0x17\n"
-        "jr $t2\n"
-        : "=r"(ret), "=r"(arg1), "=r"(arg2)
-        : "r"(arg1), "r"(arg2)
-        : "a2", "a3", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
-    return ret;
+    register volatile int n asm("t1") = 0x17;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((int(*)(const char * s1, const char * s2))0xa0)(s1, s2);
 }
 
 // doing this one in raw inline assembly would prove tricky,
@@ -42,85 +33,43 @@ static __inline__ int syscall_strcmp(const char * s1, const char * s2) {
 int syscall_printf(const char * fmt, ...);
 
 static __inline__ int syscall_addConsoleDevice() {
-    register int ret asm("v0");
-    __asm__ volatile(
-        "li $t2, 0xa0\n"
-        "li $t1, 0x98\n"
-        "jr $t2\n"
-        : "=r"(ret)
-        :
-        : "a0", "a1", "a2", "a3", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
-    return ret;
+    register volatile int n asm("t1") = 0x98;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((int(*)())0xa0)();
 }
 
 static __inline__ int syscall_addDummyConsoleDevice() {
-    register int ret asm("v0");
-    __asm__ volatile(
-        "li $t2, 0xa0\n"
-        "li $t1, 0x99\n"
-        "jr $t2\n"
-        : "=r"(ret)
-        :
-        : "a0", "a1", "a2", "a3", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
-    return ret;
+    register volatile int n asm("t1") = 0x99;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((int(*)())0xa0)();
 }
 
 static __inline__ void syscall_ioabort(int code) {
-    register int arg asm("a0") = code;
-    __asm__ volatile(
-        "li $t2, 0xa0\n"
-        "li $t1, 0xb2\n"
-        "jr $t2\n"
-        : "=r"(arg)
-        : "r"(arg)
-        : "a1", "a2", "a3", "v0", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
+    register volatile int n asm("t1") = 0xb2;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((void(*)())0xa0)(code);
 }
 
 static __inline__ void syscall_setDefaultExceptionJmpBuf() {
-    __asm__ volatile(
-        "li $t2, 0xb0\n"
-        "li $t1, 0x18\n"
-        "jr $t2\n"
-        :
-        :
-        : "a0", "a1", "a2", "a3", "v0", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
+    register volatile int n asm("t1") = 0x18;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((void(*)())0xb0)();
 }
 
 static __inline__ void syscall_putchar(int c) {
-    register int arg asm("a0") = c;
-    __asm__ volatile(
-        "li $t2, 0xb0\n"
-        "li $t1, 0x3d\n"
-        "jr $t2\n"
-        : "=r"(arg)
-        : "r"(arg)
-        : "a1", "a2", "a3", "v0", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
+    register volatile int n asm("t1") = 0x3d;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((void(*)(int))0xb0)(c);
 }
 
 static __inline__ void syscall_installExceptionHandler() {
-    __asm__ volatile(
-        "li $t2, 0xc0\n"
-        "li $t1, 0x07\n"
-        "jr $t2\n"
-        :
-        :
-        : "a0", "a1", "a2", "a3", "v0", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
+    register volatile int n asm("t1") = 0x07;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((void(*)())0xc0)();
 }
 
 static __inline__ void syscall_setupFileIO(int installTTY) {
-    register int arg asm("a0") = installTTY;
-    __asm__ volatile(
-        "li $t2, 0xc0\n"
-        "li $t1, 0x12\n"
-        "jr $t2\n"
-        : "=r"(arg)
-        : "r"(arg)
-        : "a1", "a2", "a3", "v0", "v1", "at", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "memory"
-    );
+    register volatile int n asm("t1") = 0x12;
+    __asm__ volatile("" : "=r"(n) : "r"(n));
+    return ((void(*)(int))0xc0)(installTTY);
 }
