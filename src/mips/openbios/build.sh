9cf1e4d94fe14ba8fa954f2807bcc56a4f79ad02
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2020-05-28 20:18:39 -0700

    Docker & scripts cleanup.

--- src/mips/openbios/build.sh
+++ src/mips/openbios/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-CURRENT_PATH=$(PWD)
-PROJECT_PATH=$(realpath "$CURRENT_PATH"/../../..)
-CURRENT_RELATIVE_PATH=$(realpath --relative-to="$PROJECT_PATH" "$CURRENT_PATH")
 
-docker run -it --rm -v "$PROJECT_PATH:/project" grumpycoders/pcsx-redux-build:latest make -C "$CURRENT_RELATIVE_PATH" all -j4
+ROOT=$(dirname $0)/../../..
+cd $ROOT
+ROOT=$(pwd)
+cd src/mips/openbios
 
+$ROOT/dockermake.sh all -j4
