050c9aa0d2738223a312a10ffd6d9262d9c69908
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2020-06-30 22:14:23 -0700

    Fleshing out the Lua wrapper.

--- src/core/luawrapper.cc
+++ src/core/luawrapper.cc
@@ -19,4 +19,264 @@
 
 #include "core/luawrapper.h"
 
-PCSX::Lua::Lua() : L(lua_open()) {}
+#include <assert.h>
+
+static int callwrap(lua_State* raw, lua_CFunction func) {
+    PCSX::Lua L(raw);
+    int r = 0;
+
+    try {
+        r = func(raw);
+    } catch (std::exception& e) {
+        L.error(std::string("LuaException: ") + e.what());
+    } catch (...) {
+        L.error("Unknown exception");
+    }
+    return r;
+}
+
+PCSX::Lua::Lua() : L(lua_open()) {
+    assert(("Couldn't create Lua VM", L));
+    setCallWrap(callwrap);
+    push("BLUA_THREADS");
+    newtable();
+    settable(LUA_REGISTRYINDEX);
+}
+
+PCSX::Lua& PCSX::Lua::operator=(Lua&& oL) noexcept {
+    if (this == &oL) return *this;
+
+    assert(("Can't assign a Lua VM to another one.", !L));
+
+    L = oL.L;
+    oL.L = nullptr;
+
+    return *this;
+}
+
+void PCSX::Lua::close() {
+    assert(("Can't close an already closed VM", L));
+
+    lua_close(L);
+    L = nullptr;
+}
+
+void PCSX::Lua::open_base() {
+    int n = gettop();
+    luaopen_base(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::open_table() {
+    int n = gettop();
+    luaopen_table(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::open_string() {
+    int n = gettop();
+    luaopen_string(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::open_math() {
+    int n = gettop();
+    luaopen_math(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::open_debug() {
+    int n = gettop();
+    luaopen_debug(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::open_bit() {
+    int n = gettop();
+    luaopen_bit(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::open_jit() {
+    int n = gettop();
+    luaopen_jit(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::open_ffi() {
+    int n = gettop();
+    luaopen_ffi(L);
+    while (n < gettop()) pop();
+}
+
+void PCSX::Lua::setCallWrap(lua_CallWrapper wrapper) {
+    push((void*)wrapper);
+    luaJIT_setmode(L, -1, LUAJIT_MODE_WRAPCFUNC | LUAJIT_MODE_ON);
+    pop();
+}
+
+void PCSX::Lua::declareFunc(const char* name, lua_CFunction f, int i) {
+    checkstack(2);
+    lua_pushstring(L, name);
+    lua_pushcfunction(L, f);
+    if ((i < 0) && (i > LUA_REGISTRYINDEX)) i += 2;
+    lua_settable(L, i);
+}
+
+void PCSX::Lua::call(const char* f, int i, int nargs) {
+    checkstack(1);
+    lua_pushstring(L, f);
+    lua_gettable(L, i);
+    lua_insert(L, -1 - nargs);
+    int r = lua_resume(L, nargs);
+
+    if ((r == LUA_YIELD) || (r == 0)) return;
+
+    pushLuaContext();
+
+    switch (r) {
+        case LUA_ERRRUN:
+            throw std::runtime_error("Runtime error while running LUA code.");
+        case LUA_ERRMEM:
+            throw std::runtime_error("Memory allocation error while running LUA code.");
+        case LUA_ERRERR:
+            throw std::runtime_error("Error in Error function.");
+        case LUA_ERRSYNTAX:
+            throw std::runtime_error("Syntax error in Lua code.");
+        default:
+            throw std::runtime_error(std::string("Unknow error while running LUA code (err code: ") +
+                                     std::to_string(r) + ")");
+    }
+}
+
+void PCSX::Lua::pcall(int nargs) {
+    int r = lua_pcall(L, nargs, LUA_MULTRET, 0);
+
+    if (r == 0) return;
+
+    pushLuaContext();
+
+    switch (r) {
+        case LUA_ERRRUN:
+            throw std::runtime_error("Runtime error while running LUA code.");
+        case LUA_ERRMEM:
+            throw std::runtime_error("Memory allocation error while running LUA code.");
+        case LUA_ERRERR:
+            throw std::runtime_error("Error in Error function.");
+        case LUA_ERRSYNTAX:
+            throw std::runtime_error("Syntax error in Lua code.");
+        default:
+            throw std::runtime_error(std::string("Unknow error while running LUA code (err code: ") +
+                                     std::to_string(r) + ")");
+    }
+}
+
+void PCSX::Lua::settable(int i, bool raw) {
+    if (raw) {
+        lua_rawset(L, i);
+    } else {
+        lua_settable(L, i);
+    }
+}
+
+void PCSX::Lua::gettable(int i, bool raw) {
+    if (raw) {
+        lua_rawget(L, i);
+    } else {
+        lua_gettable(L, i);
+    }
+}
+
+void PCSX::Lua::getglobal(const char* name) {
+    push(name);
+    gettable(LUA_GLOBALSINDEX);
+}
+
+void PCSX::Lua::pushLuaContext() {
+    std::string whole_msg;
+    struct lua_Debug ar;
+    bool got_error = false;
+    int level = 0;
+
+    do {
+        if (lua_getstack(L, level, &ar) == 1) {
+            if (lua_getinfo(L, "nSl", &ar) != 0) {
+                push(std::string("at ") + ar.source + ":" + std::to_string(ar.currentline) + " (" +
+                     (ar.name ? ar.name : "[top]") + ")");
+            } else {
+                got_error = true;
+            }
+        } else {
+            got_error = true;
+        }
+        level++;
+    } while (!got_error);
+}
+
+void PCSX::Lua::error(const char* msg) {
+    push(msg);
+
+    if (yielded()) {
+        pushLuaContext();
+
+        throw std::runtime_error("Runtime error while running yielded C code.");
+    } else {
+        lua_error(L);
+    }
+}
+
+bool PCSX::Lua::isobject(int i) {
+    bool r = false;
+    if (istable(i)) {
+        push("__obj");
+        gettable(i);
+        r = isuserdata();
+        pop();
+    } else {
+        r = isnil(i);
+    }
+    return r;
+}
+
+std::string PCSX::Lua::tostring(int i) {
+    switch (type(i)) {
+        case LUA_TNIL:
+            return "(nil)";
+        case LUA_TBOOLEAN:
+            return toboolean(i) ? "true" : "false";
+        case LUA_TNUMBER:
+            return std::to_string(tonumber(i));
+        default: {
+            size_t l;
+            const char* const r = lua_tolstring(L, i, &l);
+            return std::string(r, l);
+        }
+    }
+    return "<lua-NULL>";
+}
+
+std::string PCSX::Lua::escapeString(const std::string& s) {
+    std::string r = "";
+    int i;
+    for (i = 0; i < s.size(); i++) {
+        switch (s[i]) {
+            case '"':
+            case '\\':
+                r += '\\';
+                r += s[i];
+                break;
+            case '\n':
+                r += "\\n";
+                break;
+            case '\r':
+                r += "\\r";
+                break;
+            case '\0':
+                r += "\\000";
+                break;
+            default:
+                r += s[i];
+        }
+    }
+    return r;
+}
--- src/core/luawrapper.h
+++ src/core/luawrapper.h
@@ -21,18 +21,154 @@
 
 #include <string>
 
-#include "lua.hpp"
-
 #include "core/psxemulator.h"
+#include "lua.hpp"
 
 namespace PCSX {
 
 class Lua {
   public:
-	Lua();
+    typedef int (*openlualib_t)(lua_State* L);
+
+    Lua();
+    Lua(lua_State* L) : L(L) {}
+    Lua(Lua&& oL) noexcept : L(oL.L) { oL.L = nullptr; }
+    Lua(const Lua& oL) : L(oL.L) {}
+
+    Lua& operator=(Lua&& oL) noexcept;
+
+    typedef int (*lua_CallWrapper)(lua_State*, lua_CFunction);
+
+    int ref(int t = -2) { return luaL_ref(L, t); }
+    void unref(int ref, int t = -1) { luaL_unref(L, t, ref); }
+
+    void close();
+    void open_base();
+    void open_table();
+    void open_string();
+    void open_math();
+    void open_debug();
+    void open_bit();
+    void open_jit();
+    void open_ffi();
+
+    int wrap_open(openlualib_t open) {
+        int n = gettop();
+        int r = open(L);
+        while (n < gettop()) pop();
+        return r;
+    }
+    void openlib(const std::string& libname, const struct luaL_reg* l, int nup) {
+        luaL_openlib(L, libname.c_str(), l, nup);
+    }
+
+    void setCallWrap(lua_CallWrapper wrapper);
+    void declareFunc(const char* funcName, lua_CFunction f, int tableIdx = LUA_GLOBALSINDEX);
+
+    void call(const char* funcName, int tableIdx = LUA_GLOBALSINDEX, int nArgs = 0);
+    void pcall(int nArgs = 0);
+
+    void push() {
+        checkstack();
+        lua_pushnil(L);
+    }
+    void push(lua_Number n) {
+        checkstack();
+        lua_pushnumber(L, n);
+    }
+    void push(const std::string& s) {
+        checkstack();
+        lua_pushlstring(L, s.c_str(), s.length());
+    }
+    void push(bool b) {
+        checkstack();
+        lua_pushboolean(L, b);
+    }
+    template <size_t S>
+    void push(const char (&str)[S]) {
+        checkstack();
+        lua_pushlstring(L, str, S - 1);
+    }
+    void push(const char* str, ssize_t size = -1) {
+        if (size < 0) size = strlen(str);
+        checkstack();
+        lua_pushlstring(L, str, size);
+    }
+    void push(void* p) {
+        checkstack();
+        lua_pushlightuserdata(L, p);
+    }
+    void push(lua_CFunction f, int n = 0) {
+        checkstack();
+        lua_pushcclosure(L, f, n);
+    }
+    void pop(int idx = 1) { lua_pop(L, idx); }
+    int checkstack(int extra = 1) { return lua_checkstack(L, extra); }
+
+    int next(int t = -2) { return lua_next(L, t); }
+    void copy(int i = -1) {
+        checkstack();
+        lua_pushvalue(L, i);
+    }
+    void remove(int i = 1) { lua_remove(L, i); }
+    void insert(int i = 1) {
+        checkstack();
+        lua_insert(L, i);
+    }
+    void replace(int i = 1) { lua_replace(L, i); }
+    void newtable() {
+        checkstack();
+        lua_newtable(L);
+    }
+    void* newuser(size_t s) {
+        checkstack();
+        return lua_newuserdata(L, s);
+    }
+    void settable(int tableIdx = -3, bool raw = false);
+    void gettable(int tableIdx = -2, bool raw = false);
+    void rawseti(int idx, int tableIdx = -2) { lua_rawseti(L, tableIdx, idx); }
+    void rawgeti(int idx, int tableIdx = -1) { lua_rawgeti(L, tableIdx, idx); }
+    void setvar() { lua_settable(L, LUA_GLOBALSINDEX); }
+    int gettop() { return lua_gettop(L); }
+    void getglobal(const char* name);
+    void pushLuaContext();
+    void error(const char* msg);
+    void error(const std::string& msg) { error(msg.c_str()); }
+
+    int type(int i = -1) { return lua_type(L, i); }
+    bool isnil(int i = -1) { return lua_isnil(L, i); }
+    bool isboolean(int i = -1) { return lua_isboolean(L, i); }
+    bool isnumber(int i = -1) { return lua_isnumber(L, i); }
+    bool isstring(int i = -1) { return lua_isstring(L, i); }
+    bool istable(int i = -1) { return lua_istable(L, i); }
+    bool isfunction(int i = -1) { return lua_isfunction(L, i); }
+    bool iscfunction(int i = -1) { return lua_iscfunction(L, i); }
+    bool isuserdata(int i = -1) { return lua_isuserdata(L, i); }
+    bool islightuserdata(int i = -1) { return lua_islightuserdata(L, i); }
+    bool isobject(int i = -1);
+
+    int upvalue(int i) { return lua_upvalueindex(i); }
+
+    bool toboolean(int i = -1) { return lua_toboolean(L, i); }
+    lua_Number tonumber(int i = -1) { return lua_tonumber(L, i); }
+    std::string tostring(int i = -1);
+    lua_CFunction tocfunction(int i = -1) { return lua_tocfunction(L, i); }
+    void* touserdata(int i = -1) { return lua_touserdata(L, i); }
+
+    std::string escapeString(const std::string&);
+    int yield(int nresults = 0) { return lua_yield(L, nresults); }
+    bool yielded() { return lua_status(L) == LUA_YIELD; }
+    int getmetatable(int i = -1) {
+        checkstack();
+        return lua_getmetatable(L, i);
+    }
+    int setmetatable(int i = -2) { return lua_setmetatable(L, i); }
+    int sethook(lua_Hook func, int mask, int count) { return lua_sethook(L, func, mask, count); }
+
+    lua_State* getState() { return L; }
 
   private:
-	lua_State* L;
+    lua_State* L;
 };
 
-}
+}  // namespace PCSX
