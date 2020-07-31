6180428497d0caf59c3157b0b86fb0965ac9831e
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2020-07-31 08:04:14 -0700

    Removing copypasta hack.

--- third_party/imgui_lua_bindings/imgui_lua_bindings.cpp
+++ third_party/imgui_lua_bindings/imgui_lua_bindings.cpp
@@ -440,22 +440,6 @@ static void PushImguiEnums(lua_State* lState, const char* tableName) {
 	lua_rawset(lState, -3);
 };
 
-static void luaL_setfuncs(lua_State* L, const luaL_Reg* l, int nup) {
-	luaL_checkstack(L, nup, "too many upvalues");
-	for (; l->name != NULL; l++) {  /* fill the table with given functions */
-		if (l->func == NULL)  /* place holder? */
-			lua_pushboolean(L, 0);
-		else {
-			int i;
-			for (i = 0; i < nup; i++)  /* copy upvalues to the top */
-				lua_pushvalue(L, -nup);
-			lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
-		}
-		lua_setfield(L, -(nup + 2), l->name);
-	}
-	lua_pop(L, nup);  /* remove upvalues */
-}
-
 void LoadImguiBindings(lua_State* lState) {
 	lua_newtable(lState);
 	luaL_setfuncs(lState, imguilib, 0);
