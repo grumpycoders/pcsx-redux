7c1c1066ef1a4001b0863c557108556528e58a60
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2021-02-11 22:58:53 -0800

    Better demo settings.

--- src/mips/modplayer/demo.c
+++ src/mips/modplayer/demo.c
@@ -48,11 +48,24 @@ void waitVSync() {
 
 // Don't do this: it's not going to be able to pickup tempo
 // changes properly, and it will be too fast on PAL.
-#define POLL_ON_VSYNC
+//#define POLL_ON_VSYNC
 
 void main() {
     printf("Loading MOD:\'%s\'\n", _binary_timewarped_hit_start);
+    // The right way is to use timer1 to call MOD_Poll periodically,
+    // at the right frequency, independently of the current video
+    // mode, and will also properly tolerate the tempo changes.
+#ifndef POLL_ON_VSYNC
+    int wasInCS = enterCriticalSection();
+    syscall_setDefaultExceptionJmpBuf();
+#endif
     MOD_Load((struct MODFileFormat*)_binary_timewarped_hit_start);
+#ifndef POLL_ON_VSYNC
+    syscall_enableTimerIRQ(1);
+    uint32_t event = syscall_openEvent(0xf2000001, 2, 0x1000, MOD_Poll);
+    syscall_enableEvent(event);
+    if (!wasInCS) leaveCriticalSection();
+#endif
     printf("%02d Channels, %02d Orders\n", MOD_Channels, MOD_SongLength);
     unsigned row = 0xffffffff;
     unsigned order = 0xffffffff;
@@ -65,6 +78,10 @@ void main() {
             printf("Row: %02d, Order: %02d, Pattern: %02d\n", row, order, pattern);
         }
         waitVSync();
+        // If we really want to synchronize the music to vblank, we can
+        // just do that instead of using the event callback stuff, but it
+        // will play at different speed depending on video mode, and it
+        // won't handle any tempo change from the track.
 #ifdef POLL_ON_VSYNC
         MOD_Poll();
 #endif
