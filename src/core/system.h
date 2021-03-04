21fe6786ba2621ffcb4ef86a7618c0b099e28d8b
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2021-03-03 18:39:02 -0800

    Adding more locales, and tweaking existing ones.

--- src/core/system.h
+++ src/core/system.h
@@ -23,6 +23,7 @@
 #include <stdarg.h>
 
 #include <filesystem>
+#include <limits>
 #include <map>
 #include <string>
 #include <vector>
@@ -161,6 +162,17 @@ class System {
     }
 
     std::filesystem::path getBinDir() { return m_binDir; }
+
+    // needs to be odd, and is a replica of ImGui's range tables
+    enum class Range {
+        KOREAN = 1,
+        JAPANESE = 3,
+        CHINESE_FULL = 5,
+        CHINESE_SIMPLIFIED = 7,
+        CYRILLIC = 9,
+        THAI = 11,
+        VIETNAMESE = 13,
+    };
 
   private:
     std::map<uint64_t, std::string> m_i18n;
