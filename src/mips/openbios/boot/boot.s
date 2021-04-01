2d985aab7365cfb990b8ed05525b306d5dc63f9e
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2021-03-31 23:24:49 -0700

    Trying to fix chicken and egg issue with flushCache ptr

--- src/mips/openbios/boot/boot.s
+++ src/mips/openbios/boot/boot.s
@@ -210,12 +210,6 @@ bss_init_skip:
 stop:
     b     stop
 
-    .section .bss
-    .align 2
-flushCacheBiosPtr:
-    .long  0
-
-    .section .text, "ax", @progbits
     .set noreorder
     .global _cartBoot
     .global cartBootCop0Hook
@@ -235,20 +229,21 @@ _cartBoot:
     lw    $t3, (%lo(cartBootCop0Hook)+0x0c)($t9)
     lw    $t4, (%lo(cartBootCop0Hook)+0x10)($t9)
     lw    $t5, (%lo(cartBootCop0Hook)+0x14)($t9)
-    lw    $t6, (%lo(cartBootCop0Hook)+0x18)($t9)
     sw    $t0, 0x40($0)
     sw    $t1, 0x44($0)
     sw    $t2, 0x48($0)
     sw    $t3, 0x4c($0)
     sw    $t4, 0x50($0)
-    sw    $t5, 0x54($0)
+    /* ironically, what we just did technically requires
+       calling flushCache, but since our whole point here
+       is to grab its pointer, we obviously cannot */
     jr    $ra
-    sw    $t6, 0x58($0)
+    sw    $t5, 0x54($0)
 
 cartBootCop0Hook:
     lw    $t0, 0x310($0)
     la    $t1, _reset
-    sw    $t0, flushCacheBiosPtr
+    sw    $t0, 0x5c($0)
     j     $t1
     mtc0  $0, $7
 
@@ -256,7 +251,7 @@ cartBootCop0Hook:
     .type flushCacheFromRealBios, @function
 
 flushCacheFromRealBios:
-    lw    $t0, flushCacheBiosPtr
+    lw    $t0, 0x5c($0)
     nop
     jr    $t0
     nop
