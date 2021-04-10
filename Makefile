fc03ec3788fbea8b70e9ff7c05727ad147c82ce6
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2021-04-10 11:45:28 -0700

    Skeleton for ps1-packer.

--- Makefile
+++ Makefile
@@ -27,7 +27,7 @@ CPPFLAGS += -Ithird_party/libelfin
 CPPFLAGS += -Ithird_party/luajit/src
 CPPFLAGS += -Ithird_party/luv/src
 CPPFLAGS += -Ithird_party/luv/deps/lua-compat-5.3/c-api
-CPPFLAGS += -Ithird_party/uvw/src
+CPPFLAGS += -Ithird_party/ucl -Ithird_party/ucl/include
 CPPFLAGS += -Ithird_party/zstr/src
 CPPFLAGS += -g
 CPPFLAGS += -DIMGUI_IMPL_OPENGL_LOADER_GL3W -DIMGUI_ENABLE_FREETYPE
@@ -88,6 +88,7 @@ SRCS += third_party/ImGuiColorTextEdit/TextEditor.cpp
 SRCS += third_party/http-parser/http_parser.c
 SRCS += third_party/luv/src/luv.c
 SRCS += third_party/tracy/TracyClient.cpp
+SRCS += third_party/ucl/src/n2e_99.c third_party/ucl/src/alloc.c
 OBJECTS := $(patsubst %.c,%.o,$(filter %.c,$(SRCS)))
 OBJECTS += $(patsubst %.cc,%.o,$(filter %.cc,$(SRCS)))
 OBJECTS += $(patsubst %.cpp,%.o,$(filter %.cpp,$(SRCS)))
@@ -166,6 +167,9 @@ runtests: pcsx-redux-tests
 
 psyq-obj-parser: $(NONMAIN_OBJECTS) tools/psyq-obj-parser/psyq-obj-parser.cc
 	$(LD) -o $@ $(NONMAIN_OBJECTS) $(CPPFLAGS) $(CXXFLAGS) $(LDFLAGS) tools/psyq-obj-parser/psyq-obj-parser.cc -Ithird_party/ELFIO
+
+ps1-packer: $(NONMAIN_OBJECTS) tools/ps1-packer/ps1-packer.cc
+	$(LD) -o $@ $(NONMAIN_OBJECTS) $(CPPFLAGS) $(CXXFLAGS) $(LDFLAGS) tools/ps1-packer/ps1-packer.cc
 
 .PHONY: all dep clean gitclean regen-i18n runtests
 
---
+++ tools/ps1-packer/ps1-packer.cc
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *   Copyright (C) 2021 PCSX-Redux authors                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.           *
+ ***************************************************************************/
+
+#include "flags.h"
+#include "fmt/format.h"
+#include "support/file.h"
+#include "ucl/ucl.h"
+
+int main(int argc, char** argv) {
+    flags::args args(argc, argv);
+    auto output = args.get<std::string>("o");
+
+    auto inputs = args.positional();
+    const bool asksForHelp = args.get<bool>("h").value_or(false);
+    const bool hasOutput = output.has_value();
+    const bool oneInput = inputs.size() == 1;
+    const bool shell = args.get<bool>("shell").value_or(false);
+    const bool before = args.get<bool>("before").value_or(false);
+    if (asksForHelp || !oneInput || !hasOutput) {
+        fmt::print(R"(
+Usage: {} input.ps-exe [-h] [-shell] [-before] -o output.ps-exe
+  input.ps-exe      mandatory: specify the input ps-exe file.
+  -o output.ps-exe  mandatory: name of the output file.
+  -h                displays this help information and exit.
+  -shell            adds a kernel reset stub; see documentation for details.
+  -before           places the decompression routine before instead of after;
+                    see documentation for details.
+)",
+                   argv[0]);
+        return -1;
+    }
+
+    int ret = 0;
+
+    for (auto& input : inputs) {
+        PCSX::File* file = new PCSX::File(input);
+        if (file->failed()) {
+            fmt::print("Unable to open file: {}\n", input);
+            ret = -1;
+        } else {
+            ucl_nrv2e_99_compress(nullptr, 0, nullptr, 0, nullptr, 9, nullptr, nullptr);
+        }
+        delete file;
+    }
+
+    return ret;
+}
