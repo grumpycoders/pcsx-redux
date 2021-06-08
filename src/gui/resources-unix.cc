0841dd77e4d09f19de2188d439bad7c3bf540cea
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2021-06-07 18:19:22 -0700

    Better resource management.

--- src/gui/resources-unix.cc
+++ src/gui/resources-unix.cc
@@ -25,53 +25,34 @@
 void PCSX::Resources::loadIcon(std::function<void(const uint8_t*, uint32_t)> process) {
     std::filesystem::path fname = "pcsx-redux.ico";
     std::filesystem::path dir = "resources";
-    File* ico = nullptr;
-    ico = new File(fname);
-    if (ico->failed()) {
-        delete ico;
-        ico = new File(dir / fname);
+    std::unique_ptr<File> ico;
+
+    g_system->loadResource(
+        [&ico](const std::filesystem& filename) {
+            std::unique_ptr<File> newFile(new File(path));
+            ico.swap(newFile);
+            return !ico.failed();
+        },
+        "pcsx-redux.ico", "resources", "resources");
+
+    if (ico->failed()) return;
+    if (ico->read<uint16_t>() != 0) return;
+    if (ico->read<uint16_t>() != 1) return;
+    uint16_t count = ico->read<uint16_t>();
+    struct {
+        uint32_t size, offset;
+    } info[count];
+    for (unsigned i = 0; i < count; i++) {
+        ico->read<uint32_t>();
+        ico->read<uint32_t>();
+        info[i].size = ico->read<uint32_t>();
+        info[i].offset = ico->read<uint32_t>();
     }
-    if (ico->failed()) {
-        delete ico;
-        ico = new File(g_system->getBinDir() / fname);
+    for (unsigned i = 0; i < count; i++) {
+        ico->seek(info[i].offset, SEEK_SET);
+        auto slice = ico->read(info[i].size);
+        process(reinterpret_cast<const uint8_t*>(slice.data()), slice.size());
     }
-    if (ico->failed()) {
-        delete ico;
-        ico = new File(g_system->getBinDir() / dir / fname);
-    }
-    if (ico->failed()) {
-        delete ico;
-        ico = new File(std::filesystem::current_path() / fname);
-    }
-    if (ico->failed()) {
-        delete ico;
-        ico = new File(std::filesystem::current_path() / dir / fname);
-    }
-    if (ico->failed()) {
-        delete ico;
-        ico = new File(g_system->getBinDir() / ".." / "share" / "pcsx-redux" / fname);
-    }
-    do {
-        if (ico->failed()) break;
-        if (ico->read<uint16_t>() != 0) break;
-        if (ico->read<uint16_t>() != 1) break;
-        uint16_t count = ico->read<uint16_t>();
-        struct {
-            uint32_t size, offset;
-        } info[count];
-        for (unsigned i = 0; i < count; i++) {
-            ico->read<uint32_t>();
-            ico->read<uint32_t>();
-            info[i].size = ico->read<uint32_t>();
-            info[i].offset = ico->read<uint32_t>();
-        }
-        for (unsigned i = 0; i < count; i++) {
-            ico->seek(info[i].offset, SEEK_SET);
-            auto slice = ico->read(info[i].size);
-            process(reinterpret_cast<const uint8_t*>(slice.data()), slice.size());
-        }
-    } while (false);
-    delete ico;
 }
 
 #endif
