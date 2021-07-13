fed8358f9dc19917d1813b3147b8351c653de263
Nicolas Pixel Noble <pixel@nobis-crew.org>
2021-07-12 21:42:00 -0700

    Upgrading to gcc 11.1 wherever possible.

--- tools/macos-mips/mipsel-none-elf-gcc.rb
+++ tools/macos-mips/mipsel-none-elf-gcc.rb
@@ -1,8 +1,8 @@
 class MipselNoneElfGcc < Formula
   desc "The GNU compiler collection for mipsel"
   homepage "https://gcc.gnu.org"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-10.3.0/gcc-10.3.0.tar.xz"
-  sha256 "64f404c1a650f27fc33da242e1f2df54952e3963a49e06e73f6940f3223ac344"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-11.1.0/gcc-11.1.0.tar.xz"
+  sha256 "4c4a6fb8a8396059241c2e674b85b351c26a5d678274007f076957afa1cc9ddf"
 
   depends_on "gmp"
   depends_on "mipsel-none-elf-binutils"
