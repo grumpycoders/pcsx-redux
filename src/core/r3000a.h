b29b9b35435f8f14b1d704a27f27123f2acae3ee
wheremyfoodat <gponiris2004@gmail.com>
2021-08-21 02:13:56 +0300

    Add AA32/AA64 detection

--- src/core/r3000a.h
+++ src/core/r3000a.h
@@ -36,6 +36,10 @@
     #define DYNAREC_X86_32
 #elif defined(__x86_64) || defined(_M_AMD64)
     #define DYNAREC_X86_64
+#elif defined(__aarch64__) || defined(_M_ARM64) || defined(__ARM_ARCH_ISA_A64)
+    #define DYNAREC_NONE // Placeholder for AA64
+#elif defined(__arm__) || defined(_M_ARM)
+    #define DYNAREC_NONE // Placeholder for AA32
 #else
     #define DYNAREC_NONE
 #endif
