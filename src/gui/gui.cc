5520639e7df45d0369b9d7d528e545c7cd2d2f4c
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2021-08-21 20:44:39 -0700

    Actually, we need to force shader version.

--- src/gui/gui.cc
+++ src/gui/gui.cc
@@ -394,7 +394,7 @@ end)(jit.status()))
     };
     glfwSetKeyCallback(m_window, glfwKeyCallbackTrampoline);
     glfwSetJoystickCallback([](int jid, int event) { PCSX::g_emulator->m_pads->scanGamepads(); });
-    ImGui_ImplOpenGL3_Init(nullptr);
+    ImGui_ImplOpenGL3_Init(GL_SHADER_VERSION);
     glGenTextures(1, &m_VRAMTexture);
     glBindTexture(GL_TEXTURE_2D, m_VRAMTexture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
