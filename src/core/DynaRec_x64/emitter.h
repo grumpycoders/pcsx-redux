2744795361f8d898db60cf0522b79276803647f9
wheremyfoodat <gponiris2004@gmail.com>
2021-08-24 17:31:53 +0300

    Fix up MacOS memory allocation stuff.

--- src/core/DynaRec_x64/emitter.h
+++ src/core/DynaRec_x64/emitter.h
@@ -19,6 +19,9 @@
 
 #pragma once
 #include "xbyak.h"
+#ifdef __APPLE__
+#include <sys/mman.h> // For mmap
+#endif
 
 using namespace Xbyak;
 using namespace Xbyak::util;
@@ -27,8 +30,8 @@ using namespace Xbyak::util;
 constexpr uint32_t codeCacheSize = 32 * 1024 * 1024;
 
 // Allocate a bit more memory to be safe.
-// This has to be static so JIT code will be able to call C++ functions without absolute calls
-static uint8_t s_codeCache[codeCacheSize + 0x1000]; 
+// This has to be static so JIT code will be close enough to the executable to address stuff with rip-relative accesses
+alignas(4096) static uint8_t s_codeCache[codeCacheSize + 0x1000]; 
 
 struct Emitter final : public CodeGenerator {                   
     Emitter() : CodeGenerator(codeCacheSize, s_codeCache) {}
@@ -40,7 +43,13 @@ struct Emitter final : public CodeGenerator {
 
     // Tries to mark the emitter memory as readable/writeable/executable without throwing an exception.
     // Returns whether or not it succeeded
-    bool setRWX() { 
+    bool setRWX() {
+        #ifdef __APPLE__ // MacOS doesn't like marking static memory as executable the way Xbyak does, so we do it ourselves
+        return mmap(s_codeCache, codeCacheSize + 0x1000, PROT_READ | PROT_WRITE | PROT_EXEC,
+                    MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1,
+                    0) != MAP_FAILED;
+        #endif
+
         return setProtectMode(PROTECT_RWE, false);
     }
 };
