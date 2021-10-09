476268d9a557e161db0bcf42e807b0bd100addd8
wheremyfoodat <gponiris2004@gmail.com>
2021-10-09 22:14:37 +0300

    Lowercase letters in hex values.

--- src/core/psxmem.cc
+++ src/core/psxmem.cc
@@ -459,11 +459,11 @@ void *PCSX::Memory::psxMemPointerRead(uint32_t address) {
         else {
             switch (address) { // IO regs that are safe to read from directly
                 case 0x1f801080: case 0x1f801084: case 0x1f801088: case 0x1f801090:
-                case 0x1f801094: case 0x1f801098: case 0x1f8010A0: case 0x1f8010A4:
-                case 0x1f8010A8: case 0x1f8010B0: case 0x1f8010B4: case 0x1f8010B8:
-                case 0x1f8010C0: case 0x1f8010C4: case 0x1f8010C8: case 0x1f8010D0:
-                case 0x1f8010D4: case 0x1f8010D8: case 0x1f8010E0: case 0x1f8010E4:
-                case 0x1f8010E8: case 0x1f801070: case 0x1f801074: case 0x1f8010f0:
+                case 0x1f801094: case 0x1f801098: case 0x1f8010a0: case 0x1f8010a4:
+                case 0x1f8010a8: case 0x1f8010b0: case 0x1f8010b4: case 0x1f8010b8:
+                case 0x1f8010c0: case 0x1f8010c4: case 0x1f8010c8: case 0x1f8010d0:
+                case 0x1f8010d4: case 0x1f8010d8: case 0x1f8010e0: case 0x1f8010e4:
+                case 0x1f8010e8: case 0x1f801070: case 0x1f801074: case 0x1f8010f0:
                 case 0x1f8010f4:
                     return &g_psxH[address & 0xffff];
                     
@@ -489,9 +489,9 @@ void *PCSX::Memory::psxMemPointerWrite(uint32_t address) {
         else {
             switch (address) { // IO regs that are safe to write to directly
                 case 0x1f801080: case 0x1f801084: case 0x1f801090: case 0x1f801094:
-                case 0x1f8010A0: case 0x1f8010A4: case 0x1f8010B0: case 0x1f8010B4:
-                case 0x1f8010C0: case 0x1f8010C4: case 0x1f8010D0: case 0x1f8010D4:
-                case 0x1f8010E0: case 0x1f8010E4: case 0x1f801074: case 0x1f8010f0:
+                case 0x1f8010a0: case 0x1f8010a4: case 0x1f8010b0: case 0x1f8010b4:
+                case 0x1f8010c0: case 0x1f8010c4: case 0x1f8010d0: case 0x1f8010d4:
+                case 0x1f8010e0: case 0x1f8010e4: case 0x1f801074: case 0x1f8010f0:
                     return &g_psxH[address & 0xffff];
                     
                 default:
