79279a8ca7feb9934891cb3bea2890eb67e03ec5
wheremyfoodat <gponiris2004@gmail.com>
2021-11-18 01:57:16 +0200

    Slightly safer recompiler memory handling

--- src/core/DynaRec_x64/emitter.h
+++ src/core/DynaRec_x64/emitter.h
@@ -31,18 +31,19 @@ using namespace Xbyak;
 using namespace Xbyak::util;
 
 // Allocate 32MB for the code cache. This might be big, but better safe than sorry
-constexpr uint32_t codeCacheSize = 32 * 1024 * 1024;
+constexpr size_t codeCacheSize = 32 * 1024 * 1024;
+constexpr size_t allocSize = codeCacheSize + 0x1000;
 
 // Allocate a bit more memory to be safe.
 // This has to be static so JIT code will be close enough to the executable to address stuff with rip-relative accesses
-alignas(4096) static uint8_t s_codeCache[codeCacheSize + 0x1000];
+alignas(4096) static uint8_t s_codeCache[allocSize];
 
 struct Emitter final : public CodeGenerator {
     bool hasAVX = false;
     bool hasBMI2 = false;
     bool hasLZCNT = false;
 
-    Emitter() : CodeGenerator(codeCacheSize, s_codeCache) {
+    Emitter() : CodeGenerator(allocSize, s_codeCache) {
         const auto cpu = Xbyak::util::Cpu();
 
         hasAVX = cpu.has(Xbyak::util::Cpu::tAVX);
@@ -217,7 +218,7 @@ struct Emitter final : public CodeGenerator {
     // Returns whether or not it succeeded
     bool setRWX() {
 #ifdef __APPLE__  // MacOS doesn't like marking static memory as executable the way Xbyak does, so we do it ourselves
-        return mmap(s_codeCache, codeCacheSize + 0x1000, PROT_READ | PROT_WRITE | PROT_EXEC,
+        return mmap(s_codeCache, allocSize, PROT_READ | PROT_WRITE | PROT_EXEC,
                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0) != MAP_FAILED;
 #else
         return setProtectMode(PROTECT_RWE, false);
