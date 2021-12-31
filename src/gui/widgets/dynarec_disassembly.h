4921fb0e2c0e381e635e4b4ef135032b76938209
Caleb <caldog20@gmail.com>
2021-12-31 14:42:06 -0600

    Updated Copyright Dates in Disassembler Files

--- src/gui/widgets/dynarec_disassembly.h
+++ src/gui/widgets/dynarec_disassembly.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019 PCSX-Redux authors                                 *
+ *   Copyright (C) 2021 PCSX-Redux authors                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
