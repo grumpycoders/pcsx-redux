91e2f600f4dc144e7c43510775b9d6681ed1a7c5
Caleb <caldog20@gmail.com>
2022-01-29 15:11:11 -0600

    [New Branch Prep] Added docker aarch64 cross-compiler files

---
+++ dockermake-cross.bat
@@ -0,0 +1,23 @@
+@echo off
+set OLDCWD=%cd%
+cd %~dp0
+set ROOT=%cd%
+cd %OLDCWD%
+
+set REL=%OLDCWD%
+
+SETLOCAL EnableDelayedExpansion
+
+set MAT=
+set UPP=
+for /f "tokens=*" %%a in ('echo.%ROOT:\=^&echo.%') do (
+    set SUB=!SUB!%%a\
+    call set TMP=%%src:!SUB!=%%
+    set MAT=!SUB!
+)
+set REL=%UPP%!REL:%MAT%=!
+if "!REL!" EQU "!ROOT!" (set REL=) ELSE (set "REL=!REL:\=/!")
+set REL=/!REL!
+
+docker build -t aarch64cross .\tools\build-cross
+docker run --rm --env-file "%ROOT%/env.list" -i -w"/project%REL%" -v "%ROOT%:/project" aarch64cross make --makefile=Makefile-cross-aa64 %*
