ea67351ef0d79a98c793e94f2801d79b006d75a0
Caleb <caldog20@gmail.com>
2022-01-29 16:44:55 -0600

    Fixed dates in disclaimer since this was all re-implemented in the new year. Removed new-lines from end of files

--- src/core/DynaRec_aa64/gte_aa64.cc
+++ src/core/DynaRec_aa64/gte_aa64.cc
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2021 PCSX-Redux authors                                 *
+ *   Copyright (C) 2022 PCSX-Redux authors                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
