5a25b033731a362e0b23299c0cba6ad8e4c90d11
Caleb <caldog20@gmail.com>
2022-01-29 16:37:24 -0600

    [AA64 JIT] Recompiler infrastructure
    [Recompiler] DynaRecCPU::Init()
    
    [Recompiler] DynaRecCPU::Shutdown()
    
    [Recompiler] DynaRecCPU signalShellReached & flushCache definitions

--- src/core/DynaRec_aa64/recompiler.cc
+++ src/core/DynaRec_aa64/recompiler.cc
@@ -21,10 +21,84 @@
 
 #if defined(DYNAREC_AA64)
 
+bool DynaRecCPU::Init() {
+    // Initialize recompiler memory
+    // Check for 8MB RAM expansion
+    const bool ramExpansion = PCSX::g_emulator->settings.get<PCSX::Emulator::Setting8MB>();
+    m_ramSize = ramExpansion ? 0x800000 : 0x200000;
+    const auto biosSize = 0x80000;
+
+    // The amount of 64KB RAM pages. 0x80 with the ram expansion, 0x20 otherwise
+    const int ramPages = m_ramSize >> 16;
+
+    // Split the 32-bit address space into 64KB pages, so 0x10000 pages in total
+    m_recompilerLUT = new DynarecCallback*[0x10000]();
+
+    // Instructions need to be on 4-byte boundaries. So the amount of valid block entrypoints
+    // in a region of memory is REGION_SIZE / 4
+    m_ramBlocks = new DynarecCallback[m_ramSize / 4];
+    m_biosBlocks = new DynarecCallback[biosSize / 4];
+    m_dummyBlocks = new DynarecCallback[0x10000 / 4];  // Allocate one page worth of dummy blocks
+
+    gen.Reset(); // Reset code generator
+
+    for (int page = 0; page < 0x10000; page++) {  // Default all pages to dummy blocks
+        m_recompilerLUT[page] = &m_dummyBlocks[0];
+    }
+
+    // For every 64KB page of memory, we can have 64*1024/4 unique blocks = 0x4000
+    // Hence the multiplications below
+    for (int page = 0; page < ramPages; page++) {          // Map RAM to the recompiler LUT
+        const auto pointer = &m_ramBlocks[page * 0x4000];  // Get a pointer to the page of RAM blocks
+        m_recompilerLUT[page + 0x0000] = pointer;          // Map KUSEG, KSEG0 and KSEG1 RAM respectively
+        m_recompilerLUT[page + 0x8000] = pointer;
+        m_recompilerLUT[page + 0xA000] = pointer;
+    }
+
+    for (int page = 0; page < 8; page++) {  // Map BIOS to recompiler LUT
+        const auto pointer = &m_biosBlocks[page * 0x4000];
+        m_recompilerLUT[page + 0x1FC0] = pointer;  // Map KUSEG, KSEG0 and KSEG1 BIOS respectively
+        m_recompilerLUT[page + 0x9FC0] = pointer;
+        m_recompilerLUT[page + 0xBFC0] = pointer;
+    }
+
+    if (!gen.setRWX()) {
+        PCSX::g_system->message("[Dynarec] Failed to allocate executable memory.\nTry disabling the Dynarec CPU.");
+        return false;
+    }
+    emitDispatcher();  // Emit our assembly dispatcher
+    uncompileAll();    // Mark all blocks as uncompiled
+
+    for (int i = 0; i < 0x10000 / 4; i++) {  // Mark all dummy blocks as invalid
+        m_dummyBlocks[i] = m_invalidBlock;
+    }
+
+    // Profiler not yet implemented
+//    if constexpr (ENABLE_SYMBOLS) {
+//        makeSymbols();
+//    }
+//
+//    if constexpr (ENABLE_PROFILER) {
+//        m_profiler.init();
+//    }
+
+    m_regs[0].markConst(0);  // $zero is always zero
+    return true;
+}
+
 void DynaRecCPU::Reset() {
     R3000Acpu::Reset();  // Reset CPU registers
     Shutdown();          // Deinit and re-init dynarec
     Init();
+}
+
+void DynaRecCPU::Shutdown() {
+    delete[] m_recompilerLUT;
+    delete[] m_ramBlocks;
+    delete[] m_biosBlocks;
+    delete[] m_dummyBlocks;
+
+    gen.dumpBuffer(); // TODO: Possibly move x64 dumpBuffer() method to emitter.h
 }
 
 /// Params: A program counter value
@@ -34,6 +108,13 @@ DynarecCallback* DynaRecCPU::getBlockPointer(uint32_t pc) {
     const auto offset = (pc & 0xFFFF) >> 2;  // Remove the 2 lower bits, they're guaranteed to be 0
 
     return &base[offset];
+}
+
+void DynaRecCPU::signalShellReached(DynaRecCPU* that) {
+    if (!that->m_shellStarted) {
+        that->m_shellStarted = true;
+        PCSX::g_system->m_eventBus->signal(PCSX::Events::ExecutionFlow::ShellReached{});
+    }
 }
 
 void DynaRecCPU::error() {
@@ -51,6 +132,13 @@ void DynaRecCPU::uncompileAll() {
         m_biosBlocks[i] = m_uncompiledBlock;
     }
 }
+
+void DynaRecCPU::flushCache() {
+    gen.Reset();       // Reset the emitter's code pointer and code size variables
+    emitDispatcher();  // Re-emit dispatcher
+    uncompileAll();    // Mark all blocks as uncompiled
+}
+
 
 std::unique_ptr<PCSX::R3000Acpu> PCSX::Cpus::getDynaRec() { return std::unique_ptr<PCSX::R3000Acpu>(new DynaRecCPU()); }
 
