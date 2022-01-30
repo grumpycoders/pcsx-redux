d725f239e05c4dcc9b5aec3e34b9fc83a766dd6f
Caleb <caldog20@gmail.com>
2022-01-29 21:48:01 -0600

    [Recompiler Instructions] DynaRecCPU::recSW() - Implemented

--- src/core/DynaRec_aa64/instructions.cc
+++ src/core/DynaRec_aa64/instructions.cc
@@ -111,7 +111,47 @@ void DynaRecCPU::recSRL() { throw std::runtime_error("[Unimplemented] SRL instru
 void DynaRecCPU::recSRLV() { throw std::runtime_error("[Unimplemented] SRLV instruction"); }
 void DynaRecCPU::recSUB() { throw std::runtime_error("[Unimplemented] SUB instruction"); }
 void DynaRecCPU::recSUBU() { throw std::runtime_error("[Unimplemented] SUBU instruction"); }
-void DynaRecCPU::recSW() { throw std::runtime_error("[Unimplemented] SW instruction"); }
+
+void DynaRecCPU::recSW() {
+    if (m_regs[_Rs_].isConst()) {
+        const uint32_t addr = m_regs[_Rs_].val + _Imm_;
+        const auto pointer = PCSX::g_emulator->m_psxMem->psxMemPointerWrite(addr);
+        if (pointer != nullptr) {
+            if (m_regs[_Rt_].isConst()) {
+                store<32>(m_regs[_Rt_].val, pointer);
+            } else {
+                allocateReg(_Rt_);
+                store<32>(m_regs[_Rt_].allocatedReg, pointer);
+            }
+
+            return;
+        }
+
+        if (m_regs[_Rt_].isConst()) {  // Value to write in arg2
+            gen.Mov(arg2, m_regs[_Rt_].val);
+        } else {
+            allocateReg(_Rt_);
+            gen.Mov(arg2, m_regs[_Rt_].allocatedReg);
+        }
+
+        gen.Mov(arg1, addr);  // Address to write to in arg1   TODO: Optimize
+        call(psxMemWrite32Wrapper);
+    }
+
+    else {
+        if (m_regs[_Rt_].isConst()) {  // Value to write in arg2
+            gen.Mov(arg2, m_regs[_Rt_].val);
+        } else {
+            allocateReg(_Rt_);
+            gen.Mov(arg2, m_regs[_Rt_].allocatedReg);
+        }
+
+        allocateReg(_Rs_);
+        gen.moveAndAdd(arg1, m_regs[_Rs_].allocatedReg, _Imm_);  // Address to write to in arg1   TODO: Optimize
+        call(psxMemWrite32Wrapper);
+    }
+}
+
 void DynaRecCPU::recSWL() { throw std::runtime_error("[Unimplemented] SWL instruction"); }
 void DynaRecCPU::recSWR() { throw std::runtime_error("[Unimplemented] SWR instruction"); }
 void DynaRecCPU::recSYSCALL() { throw std::runtime_error("[Unimplemented] SYSCALL instruction"); }
