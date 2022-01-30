52fb722b93f73fc0a6e604f4bb859a973c977c2c
Caleb <caldog20@gmail.com>
2022-01-30 00:14:08 -0600

    [Recompiler Instructions] recJ, recCOP0, recBNE, recADDIU, recSLL
    [Recompiler Instructions] DynaRecCPU::recSLL() - Implemented
    
    [Recompiler Instructions] DynaRecCPU::recADDIU() - Implemented
    
    [Recompiler Instructions] DynaRecCPU::recBNE() - Implemented
    
    [Recompiler Instructions] DynaRecCPU::recCOP0() handler - Implemented

--- src/core/DynaRec_aa64/instructions.cc
+++ src/core/DynaRec_aa64/instructions.cc
@@ -34,19 +34,117 @@ void DynaRecCPU::recSpecial() {
 }
 
 void DynaRecCPU::recADD() { throw std::runtime_error("[Unimplemented] ADD instruction"); }
-void DynaRecCPU::recADDIU() { throw std::runtime_error("[Unimplemented] ADDIU instruction"); }
+
+void DynaRecCPU::recADDIU() {
+    BAILZERO(_Rt_);
+    maybeCancelDelayedLoad(_Rt_);
+
+    if (_Rs_ == _Rt_) {
+        if (m_regs[_Rt_].isConst()) {
+            m_regs[_Rt_].val += _Imm_;
+        } else {
+            allocateReg(_Rt_);
+            m_regs[_Rt_].setWriteback(true);
+            switch (_Imm_) {
+                case 1:
+                    gen.Add(m_regs[_Rt_].allocatedReg, m_regs[_Rt_].allocatedReg, 1);
+                    break;
+                case -1:
+                    gen.Sub(m_regs[_Rt_].allocatedReg, m_regs[_Rt_].allocatedReg, 1);
+                    break;
+                default:
+                    gen.Add(m_regs[_Rt_].allocatedReg, m_regs[_Rt_].allocatedReg, _Imm_);
+                    break;
+            }
+        }
+    } else {
+        if (m_regs[_Rs_].isConst()) {
+            markConst(_Rt_, m_regs[_Rs_].val + _Imm_);
+        } else {
+            alloc_rs_wb_rt();
+            gen.moveAndAdd(m_regs[_Rt_].allocatedReg, m_regs[_Rs_].allocatedReg, _Imm_);
+        }
+    }
+}
+
 void DynaRecCPU::recADDU() { throw std::runtime_error("[Unimplemented] ADDU instruction"); }
 void DynaRecCPU::recAND() { throw std::runtime_error("[Unimplemented] AND instruction"); }
 void DynaRecCPU::recANDI() { throw std::runtime_error("[Unimplemented] ANDI instruction"); }
 void DynaRecCPU::recBEQ() { throw std::runtime_error("[Unimplemented] BEQ instruction"); }
 void DynaRecCPU::recBGTZ() { throw std::runtime_error("[Unimplemented] BGTZ instruction"); }
 void DynaRecCPU::recBLEZ() { throw std::runtime_error("[Unimplemented] BLEZ instruction"); }
-void DynaRecCPU::recBNE() { throw std::runtime_error("[Unimplemented] BNE instruction"); }
+
+void DynaRecCPU::recBNE() {
+    const auto target = _Imm_ * 4 + m_pc;
+    m_nextIsDelaySlot = true;
+
+    if (target == m_pc + 4) {
+        return;
+    }
+
+    if (m_regs[_Rs_].isConst() && m_regs[_Rt_].isConst()) {
+        if (m_regs[_Rs_].val != m_regs[_Rt_].val) {
+            m_pcWrittenBack = true;
+            m_stopCompiling = true;
+            gen.Mov(scratch, target);
+            gen.Str(scratch, MemOperand(contextPointer, PC_OFFSET));
+            m_linkedPC = target;
+        }
+        return;
+    } else if (m_regs[_Rs_].isConst()) {
+        allocateReg(_Rt_);
+        gen.cmpEqImm(m_regs[_Rt_].allocatedReg, m_regs[_Rs_].val);
+    } else if (m_regs[_Rt_].isConst()) {
+        allocateReg(_Rs_);
+        gen.cmpEqImm(m_regs[_Rs_].allocatedReg, m_regs[_Rt_].val);
+    } else {
+        alloc_rt_rs();
+        gen.Cmp(m_regs[_Rt_].allocatedReg, m_regs[_Rs_].allocatedReg);
+    }
+
+    m_pcWrittenBack = true;
+    m_stopCompiling = true;
+
+    gen.Mov(scratch, target); // scratch = addr if jump taken
+    gen.Mov(scratch2, m_pc + 4); // scratch2 = addr if jump not taken
+    gen.Csel(w0, scratch, scratch2, ne); // if not equal, return the jump addr into w0
+    gen.Str(w0, MemOperand(contextPointer, PC_OFFSET));
+}
+
 void DynaRecCPU::recBREAK() { throw std::runtime_error("[Unimplemented] BREAK instruction"); }
-void DynaRecCPU::recCOP0() { throw std::runtime_error("[Unimplemented] COP0 instruction"); }
+
+void DynaRecCPU::recCOP0() {
+    switch (_Rs_) {  // figure out the type of COP0 opcode
+        case 0:
+            recMFC0();
+            break;
+        case 4:
+            recMTC0();
+            break;
+        case 16:
+            recRFE();
+            break;
+        default:
+            fmt::print("Unimplemented cop0 op {}\n", _Rs_);
+            recUnknown();
+            break;
+    }
+}
+
 void DynaRecCPU::recDIV() { throw std::runtime_error("[Unimplemented] DIV instruction"); }
 void DynaRecCPU::recDIVU() { throw std::runtime_error("[Unimplemented] DIVU instruction"); }
-void DynaRecCPU::recJ() { throw std::runtime_error("[Unimplemented] J instruction"); }
+
+void DynaRecCPU::recJ() {
+    const uint32_t target = (m_pc & 0xf0000000) | (_Target_ << 2);
+    m_nextIsDelaySlot = true;
+    m_stopCompiling = true;
+    m_pcWrittenBack = true;
+
+    gen.Mov(scratch, target);
+    gen.Str(scratch, MemOperand(contextPointer, PC_OFFSET)); // Write PC
+    m_linkedPC = target;
+}
+
 void DynaRecCPU::recJAL() { throw std::runtime_error("[Unimplemented] JAL instruction"); }
 void DynaRecCPU::recJALR() { throw std::runtime_error("[Unimplemented] JALR instruction"); }
 void DynaRecCPU::recJR() { throw std::runtime_error("[Unimplemented] JR instruction"); }
@@ -102,7 +200,20 @@ void DynaRecCPU::recREGIMM() { throw std::runtime_error("[Unimplemented] REGIMM 
 void DynaRecCPU::recRFE() { throw std::runtime_error("[Unimplemented] RFE instruction"); }
 void DynaRecCPU::recSB() { throw std::runtime_error("[Unimplemented] SB instruction"); }
 void DynaRecCPU::recSH() { throw std::runtime_error("[Unimplemented] SH instruction"); }
-void DynaRecCPU::recSLL() { throw std::runtime_error("[Unimplemented] SLL instruction"); }
+
+void DynaRecCPU::recSLL() {
+    BAILZERO(_Rd_);
+    maybeCancelDelayedLoad(_Rd_);
+
+    if (m_regs[_Rt_].isConst()) {
+        markConst(_Rd_, m_regs[_Rt_].val << _Sa_);
+    } else {
+        alloc_rt_wb_rd();
+        // Was using shlImm in emitter.h - possible optimization opportunity
+        gen.Lsl(m_regs[_Rd_].allocatedReg, m_regs[_Rt_].allocatedReg, _Sa_);
+    }
+}
+
 void DynaRecCPU::recSLLV() { throw std::runtime_error("[Unimplemented] SLLV instruction"); }
 void DynaRecCPU::recSLT() { throw std::runtime_error("[Unimplemented] SLT instruction"); }
 void DynaRecCPU::recSLTI() { throw std::runtime_error("[Unimplemented] SLTI instruction"); }
