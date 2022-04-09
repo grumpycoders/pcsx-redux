b3b0a33dd72752c8043f91fca6daa5d262c1b536
wheremyfoodat <gponiris2004@gmail.com>
2022-04-09 20:42:49 +0300

    [OpenGL GPU] Get rid of geometry shader

--- src/core/OpenGL_GPU/gpu_opengl.cc
+++ src/core/OpenGL_GPU/gpu_opengl.cc
@@ -83,32 +83,8 @@ int PCSX::OpenGL_GPU::init() {
         }
     )";
 
-    static const char* geomSource = R"(
-        #version 330 core
-        layout(triangles) in;
-        layout(triangle_strip, max_vertices = 5) out;
-
-        void build_house(vec4 position) {
-            gl_Position = position + vec4(0.0, -0.4, 0.0, 0.0);  // 1:top
-            EmitVertex();
-            gl_Position = position + vec4(0.2, -0.2, 0.0, 0.0);  // 2:top-right
-            EmitVertex();
-            gl_Position = position + vec4(-0.2, -0.2, 0.0, 0.0);  // 3:top-left
-            EmitVertex();
-            gl_Position = position + vec4(0.2, 0.2, 0.0, 0.0);  // 4:bottom-right
-            EmitVertex();
-            gl_Position = position + vec4(-0.2, 0.2, 0.0, 0.0);  // 5:bottom-left
-            EmitVertex();
-            EndPrimitive();
-        }
-
-        void main() { 
-            build_house(gl_in[0].gl_Position);
-        }
-    )";
     OpenGL::Shader frag(fragSource, OpenGL::Fragment);
     OpenGL::Shader vert(vertSource, OpenGL::Vertex);
-    OpenGL::Shader geom(geomSource, OpenGL::Geometry);
     m_untexturedTriangleProgram.create({frag, vert});
     m_untexturedTriangleProgram.use();
     
