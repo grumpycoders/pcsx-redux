08b97ce0645f89dd97a816099f0aaedb8fd4c986
John Baumann <johnbaumann@live.com>
2022-04-21 16:09:38 -0500

    Fix numeric conversion that caused incorrect report of track number

--- src/core/cdrom.cc
+++ src/core/cdrom.cc
@@ -363,7 +363,8 @@ class CDRomImpl : public PCSX::CDRom {
         current = time.toLBA();
 
         for (m_curTrack = 1; m_curTrack < m_iso->getTN(); m_curTrack++) {
-            if (m_iso->getTD(m_curTrack + 1).toLBA() - current >= 150) break;
+            sect = static_cast<int>(m_iso->getTD(m_curTrack + 1).toLBA());
+            if (sect - current >= 150) break;
         }
         CDROM_LOG("Find_CurTrack *** %02d %02d\n", m_curTrack, current);
     }
