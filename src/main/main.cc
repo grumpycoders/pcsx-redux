967ed1b0f8984183f99f4680d069d0689363af56
Nicolas Noble <nicolasnoble@users.noreply.github.com>
2022-04-24 11:46:10 -0700

    Properly purging all libuv events.
    Fixes a crash on exit.

--- src/main/main.cc
+++ src/main/main.cc
@@ -108,7 +108,6 @@ class SystemImpl final : public PCSX::System {
     }
 
     virtual void purgeAllEvents() final override {
-        uv_stop(getLoop());
         uv_run(getLoop(), UV_RUN_DEFAULT);
     }
 
