09a31e5c72226e2fdad7b74414b5f62420d2d69e
wheremyfoodat <gponiris2004@gmail.com>
2022-05-08 00:31:03 +0300

    [OpenGL GPU] Fix Nvidia build

--- src/core/OpenGL_GPU/gpu_opengl.cc
+++ src/core/OpenGL_GPU/gpu_opengl.cc
@@ -176,11 +176,11 @@ int PCSX::OpenGL_GPU::init() {
            
            gl_Position = vec4(xx, yy, 1.0, 1.0);
            vertexColor = vec4(color / 255.0, 1.0);
-           texCoords = inUV + vec2(+0.5, -0.5);
+           texCoords = inUV + vec2(+0.5, +0.5);
            texpageBase = ivec2((inTexpage & 0xf) * 64, ((inTexpage >> 4) & 0x1) * 256);
            clutBase = ivec2((inClut & 0x3f) * 16, inClut >> 6);
 
-           if (inTexpage & 0x8000) { // Untextured primitive
+           if ((inTexpage & 0x8000) != 0) { // Untextured primitive
                texMode = 4;
            } else {
                texMode = (inTexpage >> 7) & 3;
