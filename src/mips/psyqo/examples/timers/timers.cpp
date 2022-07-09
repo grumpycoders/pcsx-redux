595ea5bf51304d3ad8e8ccb6d12f4df081dda115
Nicolas Pixel Noble <pixel@nobis-crew.org>
2022-07-09 16:49:31 -0700

    More commenting on the Timers example.

--- src/mips/psyqo/examples/timers/timers.cpp
+++ src/mips/psyqo/examples/timers/timers.cpp
@@ -31,6 +31,11 @@ SOFTWARE.
 #include "psyqo/scene.hh"
 #include "psyqo/simplepad.hh"
 
+// This example goes a little bit over the top, but it's a good example of
+// how to use the PSYQo library. The Pause scene probably is overkill, and
+// should rather be handled by the same scene, but this way we're properly
+// showing a scene transition and the passage of callbacks.
+
 namespace {
 
 class Timers final : public psyqo::Application {
@@ -40,6 +45,7 @@ class Timers final : public psyqo::Application {
   public:
     psyqo::Font<1> m_font;
     psyqo::SimplePad m_input;
+    bool m_initialized = false;
     bool m_paused = false;
 };
 
@@ -63,6 +69,8 @@ TimersPausedScene timersPausedScene;
 
 }  // namespace
 
+// As usual, we're initializing the GPU and only the GPU in the `prepare` method of the
+// whole application.
 void Timers::prepare() {
     psyqo::GPU::Configuration config;
     config.set(psyqo::GPU::Resolution::W320)
@@ -72,21 +80,41 @@ void Timers::prepare() {
     gpu().initialize(config);
 }
 
+// We're going to use pads to control the application. We'll use the `SimplePad` class
+// which needs to be initialized here.
 void Timers::createScene() {
-    m_font.uploadSystemFont(gpu());
-    m_input.initialize();
+    // Even though our logic isn't meant to be exiting our root scene, we're going to
+    // show how to properly protect against such case.
+    if (!m_initialized) {
+        m_font.uploadSystemFont(gpu());
+        m_input.initialize();
+        m_initialized = true;
+    }
+    // We only have one root scene, so just push it. One case where this can be
+    // useful is a "return to menu" which simply pops all of the scenes until it
+    // reaches `nullptr`, thus allowing the logic to be properly destroyed and
+    // re-created from scratch.
     pushScene(&timersScene);
 }
 
 void TimersScene::start(Scene::StartReason reason) {
+    // When our root scene begins, we're going to capture pad input.
+    // Our only reaction here will be to pause the application when
+    // the user presses the `Start` button.
     timers.m_input.setOnEvent([this](const psyqo::SimplePad::Event& event) {
         if (event.type != psyqo::SimplePad::Event::ButtonReleased) return;
+        // Note that we _could_ use `pushScene` here, but we'd be at risk of
+        // destroying the current lambda that's being executed, leading to
+        // memory corruption, if the compiler doesn't optimize it with a tail
+        // call.
         if (event.button == psyqo::SimplePad::Start) timers.m_paused = true;
     });
     if (reason == Scene::StartReason::Create) {
+        // If we are getting created, create a 500ms periodic timer too.
         m_timerId = timers.gpu().armPeriodicTimer(500'000, [this](auto) { m_fasterCounter++; });
     } else {
-        timers.gpu().pauseTimer(m_timerId);
+        // Otherwise, if we're getting resumed, we're going to resume our timer.
+        timers.gpu().resumeTimer(m_timerId);
     }
 }
 
@@ -98,19 +126,27 @@ void TimersScene::frame() {
     auto c = psyqo::Color{{.r = 255, .g = 255, .b = 255}};
     timers.m_font.printf(timers.gpu(), {{.x = 64, .y = 32}}, c, "%i.%02i", secs, frac);
     timers.m_font.printf(timers.gpu(), {{.x = 64, .y = 64}}, c, "Half-secs: %i", m_fasterCounter);
+    // The application is paused when the user presses the `Start` button. We are delegating
+    // pushing the Pause scene here to avoid potential memory corruption within the pad callback.
     if (timers.m_paused) pushScene(&timersPausedScene);
 }
 
 void TimersScene::teardown(Scene::TearDownReason reason) {
+    // When we're no longer the main scene, remove the pad callback.
     timers.m_input.setOnEvent(nullptr);
     if (reason == Scene::TearDownReason::Destroy) {
+        // If we're being popped out (which shouldn't happen due to the way our scene
+        // logic currently is setup), we're going to destroy our timer.
         timers.gpu().cancelTimer(m_timerId);
     } else {
-        timers.gpu().resumeTimer(m_timerId);
+        // Otherwise, this means we're being paused, so we're going to pause our timer.
+        timers.gpu().pauseTimer(m_timerId);
     }
 }
 
 void TimersPausedScene::start(Scene::StartReason reason) {
+    // The Paused scene only wants to unpause the application, so it's doing
+    // the same logic as the root scene, but to set `m_paused` to false instead.
     timers.m_input.setOnEvent([this](const psyqo::SimplePad::Event& event) {
         if (event.type != psyqo::SimplePad::Event::ButtonReleased) return;
         if (event.button == psyqo::SimplePad::Start) timers.m_paused = false;
@@ -120,6 +156,8 @@ void TimersPausedScene::start(Scene::StartReason reason) {
 void TimersPausedScene::frame() {
     timers.gpu().clear();
     timers.m_font.printf(timers.gpu(), {{.x = 64, .y = 32}}, {{.r = 255, .g = 255, .b = 255}}, "Paused");
+    // Similarly to the root scene, we're going to change the scene during `frame`
+    // instead of the pad callback.
     if (!timers.m_paused) popScene();
 }
 
