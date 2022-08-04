9805347dc4ecc709299763a7f6852b1b62754368
twevs <77587819+twevs@users.noreply.github.com>
2022-08-04 01:28:17 +0100

    Updated lists of MGS data types and functions.

--- export_data_types_redux.py
+++ export_data_types_redux.py
@@ -10,6 +10,13 @@ with open (root_dir + '/build/ghidra_scripts/data_types_redux.txt', 'w') as f:
     # @todo: enums, typedefs, etc.
     for data_type in dtm.getAllStructures():
         dt_info = data_type.getName() + ';'
+        print('struct: ' + dt_info)
         for component in data_type.getComponents():
-            dt_info += component.getDataType().getName() + ',' + component.getFieldName() + ',' + str(component.getLength()) + ';'
+            type_name = component.getDataType().getName()
+            print('type: ' + type_name)
+            field_name = component.getFieldName()
+            print('field name: ' + field_name)
+            field_length = str(component.getLength())
+            print('field length: ' + field_length)
+            dt_info += type_name + ',' + field_name + ',' + field_length + ';'
         f.write(dt_info + '\n')
