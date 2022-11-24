58814220c8d28f070db5ecc8f82124b324d3cde2
Nicolas Pixel Noble <pixel@nobis-crew.org>
2022-11-24 13:09:06 -0800

    Another stab at MacOS

--- src/support/coroutine.h
+++ src/support/coroutine.h
@@ -20,6 +20,7 @@
 #pragma once
 
 #ifdef __APPLE__
+// Why has Apple become the Microsoft of Software Engineering?
 #include <experimental/coroutine>
 #else
 #include <coroutine>
@@ -30,6 +31,15 @@ namespace PCSX {
 
 template <typename T = void>
 struct Coroutine {
+#ifdef __APPLE__
+    template <typename U>
+    using CoroutineHandle = std::experimental::coroutine_handle<U>;
+    using CoroutineHandleVoid = std::experimental::coroutine_handle<void>;
+#else
+    template <typename U>
+    using CoroutineHandle = std::coroutine_handle<U>;
+    using CoroutineHandleVoid = std::coroutine_handle<void>;
+#endif
     struct Empty {};
     typedef typename std::conditional<std::is_void<T>::value, Empty, T>::type SafeT;
 
@@ -40,21 +50,12 @@ struct Coroutine {
     Coroutine &operator=(Coroutine const &) = delete;
 
     struct Awaiter {
-        Awaiter(Awaiter &&other) = default;
-        Awaiter &operator=(Awaiter &&other) = default;
-        Awaiter(Awaiter const &) = default;
-        Awaiter &operator=(Awaiter const &) = default;
         constexpr bool await_ready() const noexcept { return false; }
-        void await_suspend(std::coroutine_handle<> h) {}
+        constexpr void await_suspend(CoroutineHandleVoid) const noexcept {}
         constexpr void await_resume() const noexcept {}
-
-      private:
-        Awaiter(Coroutine *coroutine) : m_coroutine(coroutine) {}
-        Coroutine *m_coroutine;
-        friend struct Coroutine;
     };
 
-    Awaiter awaiter() { return Awaiter(this); }
+    Awaiter awaiter() { return {}; }
     void resume() {
         if (!m_handle) return;
         m_handle.resume();
@@ -74,28 +75,25 @@ struct Coroutine {
 
   private:
     struct PromiseVoid {
-        Coroutine<T> get_return_object() {
-            return Coroutine{std::move(std::coroutine_handle<Promise>::from_promise(*this))};
-        }
-        std::suspend_always initial_suspend() { return {}; }
-        std::suspend_always final_suspend() noexcept { return {}; }
+        Coroutine<T> get_return_object() { return Coroutine{std::move(CoroutineHandle<Promise>::from_promise(*this))}; }
+        Awaiter initial_suspend() { return {}; }
+        Awaiter final_suspend() noexcept { return {}; }
         void unhandled_exception() {}
         template <typename From>
-        std::suspend_always yield_value(From &&from) {
+        Awaiter yield_value(From &&from) {
             return {};
         }
         void return_void() {}
     };
     struct PromiseValue {
         PromiseValue(Coroutine<T> *c) : coroutine(c) {}
-        Coroutine<T> get_return_object() {
-            return Coroutine{std::move(std::coroutine_handle<Promise>::from_promise(*this))};
-        }
-        std::suspend_always initial_suspend() { return {}; }
-        std::suspend_always final_suspend() noexcept { return {}; }
+        Coroutine<T> get_return_object() { return Coroutine{std::move(CoroutineHandle<Promise>::from_promise(*this))}; }
+        Awaiter initial_suspend() { return {}; }
+        Awaiter final_suspend() noexcept { return {}; }
         void unhandled_exception() {}
-        template <std::convertible_to<T> From>
-        std::suspend_always yield_value(From &&from) {
+        // This should be an std::convertible_to<T>, but Apple still doesn't have a fully C++-20 conformant library.
+        template <typename From>
+        Awaiter yield_value(From &&from) {
             coroutine->m_value = std::forward<From>(from);
             return {};
         }
@@ -103,8 +101,8 @@ struct Coroutine {
         Coroutine<T> coroutine = nullptr;
     };
     typedef typename std::conditional<std::is_void<T>::value, PromiseVoid, PromiseValue>::type Promise;
-    Coroutine(std::coroutine_handle<Promise> &&handle) : m_handle(std::move(handle)) {}
-    std::coroutine_handle<Promise> m_handle;
+    Coroutine(CoroutineHandle<Promise> &&handle) : m_handle(std::move(handle)) {}
+    CoroutineHandle<Promise> m_handle;
     [[no_unique_address]] SafeT m_value;
 
   public:
