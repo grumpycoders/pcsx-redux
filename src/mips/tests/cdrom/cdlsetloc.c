46f0d3e0e54921ac3665c5e298d247489587256d
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2022-12-22 22:27:45 -0800

    Adding more invalid setloc tests.

--- src/mips/tests/cdrom/cdlsetloc.c
+++ src/mips/tests/cdrom/cdlsetloc.c
@@ -52,9 +52,10 @@ CESTER_TEST(cdlSetLoc, test_instances,
     cester_assert_uint_eq(0x18, stat2);
     cester_assert_uint_eq(2, response[0]);
     cester_assert_uint_eq(1, responseSize);
-    // Typical value seems to be around 1ms.
+    // Typical value seems to be around 1ms, but has
+    // been seen to spike high from time to time.
     cester_assert_uint_ge(completeTime, 500);
-    cester_assert_uint_lt(completeTime, 2000);
+    cester_assert_uint_lt(completeTime, 6500);
     ramsyscall_printf("Basic setloc to 00:02:00, complete in %ius\n", completeTime);
 )
 
@@ -82,9 +83,8 @@ CESTER_TEST(cdlSetLocNoArgs, test_instances,
     cester_assert_uint_eq(3, response[0]);
     cester_assert_uint_eq(32, response[1]);
     cester_assert_uint_eq(2, responseSize);
-    // Typical value seems to be around 1ms.
     cester_assert_uint_ge(errorTime, 500);
-    cester_assert_uint_lt(errorTime, 1500);
+    cester_assert_uint_lt(errorTime, 6500);
     ramsyscall_printf("Invalid setloc with no args, errored in %ius\n", errorTime);
 )
 
@@ -138,12 +138,77 @@ CESTER_TEST(cdlSetLocMultiple, test_instances,
 
     // Setloc is only changing an internal state.
     // Its response time is very fast, and won't
-    // vary regardless of the location.
+    // vary regardless of the location, but can
+    // still spike to 6ms from time to time.
     cester_assert_uint_ge(time1, 500);
-    cester_assert_uint_lt(time1, 2000);
+    cester_assert_uint_lt(time1, 6500);
     cester_assert_uint_ge(time2, 500);
-    cester_assert_uint_lt(time2, 2000);
+    cester_assert_uint_lt(time2, 6500);
     cester_assert_uint_ge(time3, 500);
-    cester_assert_uint_lt(time3, 2000);
+    cester_assert_uint_lt(time3, 6500);
     ramsyscall_printf("Multiple setloc to 00:02:00, complete in %ius, %ius, %ius\n", time1, time2, time3);
+)
+
+CESTER_TEST(cdlSetLocInvalid1, test_instances,
+    int resetDone = resetCDRom();
+    cester_assert_true(resetDone);
+    if (!resetDone) return;
+
+    initializeTime();
+    CDROM_REG0 = 0;
+    CDROM_REG2 = 0;
+    CDROM_REG2 = 2;
+    CDROM_REG2 = 0x2a;
+    CDROM_REG1 = CDL_SETLOC;
+    uint32_t errorTime = waitCDRomIRQ();
+    uint8_t cause1 = ackCDRomCause();
+    uint8_t stat1 = CDROM_REG0 & ~3;
+    uint8_t response[16];
+    uint8_t responseSize = readResponse(response);
+    uint8_t stat2 = CDROM_REG0 & ~3;
+    CDROM_REG0 = 1;
+    uint8_t cause1b = CDROM_REG3_UC;
+
+    cester_assert_uint_eq(5, cause1);
+    cester_assert_uint_eq(0xe0, cause1b);
+    cester_assert_uint_eq(0x38, stat1);
+    cester_assert_uint_eq(0x18, stat2);
+    cester_assert_uint_eq(3, response[0]);
+    cester_assert_uint_eq(16, response[1]);
+    cester_assert_uint_eq(2, responseSize);
+    cester_assert_uint_ge(errorTime, 500);
+    cester_assert_uint_lt(errorTime, 6500);
+    ramsyscall_printf("Invalid setloc to 00:02:2a, errored in %ius\n", errorTime);
+)
+
+CESTER_TEST(cdlSetLocInvalid2, test_instances,
+    int resetDone = resetCDRom();
+    cester_assert_true(resetDone);
+    if (!resetDone) return;
+
+    initializeTime();
+    CDROM_REG0 = 0;
+    CDROM_REG2 = 0;
+    CDROM_REG2 = 2;
+    CDROM_REG2 = 0x79;
+    CDROM_REG1 = CDL_SETLOC;
+    uint32_t errorTime = waitCDRomIRQ();
+    uint8_t cause1 = ackCDRomCause();
+    uint8_t stat1 = CDROM_REG0 & ~3;
+    uint8_t response[16];
+    uint8_t responseSize = readResponse(response);
+    uint8_t stat2 = CDROM_REG0 & ~3;
+    CDROM_REG0 = 1;
+    uint8_t cause1b = CDROM_REG3_UC;
+
+    cester_assert_uint_eq(5, cause1);
+    cester_assert_uint_eq(0xe0, cause1b);
+    cester_assert_uint_eq(0x38, stat1);
+    cester_assert_uint_eq(0x18, stat2);
+    cester_assert_uint_eq(3, response[0]);
+    cester_assert_uint_eq(16, response[1]);
+    cester_assert_uint_eq(2, responseSize);
+    cester_assert_uint_ge(errorTime, 500);
+    cester_assert_uint_lt(errorTime, 6500);
+    ramsyscall_printf("Invalid setloc to 00:02:79, errored in %ius\n", errorTime);
 )
