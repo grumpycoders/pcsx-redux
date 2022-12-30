b8938dd0699e0d1e73e80c5d41711c77bb512e98
Nicolas Pixel Noble <pixel@nobis-crew.org>
2022-12-30 10:16:36 -0800

    Adding more racy tests.

--- src/mips/tests/cdrom/race.c
+++ src/mips/tests/cdrom/race.c
@@ -43,6 +43,8 @@ CESTER_TEST(raceGetLocPAndNop, test_instances,
     CDROM_REG0 = 0;
     CDROM_REG1 = CDL_GETLOCP;
     CDROM_REG1 = CDL_NOP;
+    uint8_t ctrl0 = CDROM_REG0 & ~3;
+
     uint32_t ackTime = waitCDRomIRQ();
     uint8_t cause1 = ackCDRomCause();
     uint8_t ctrl1 = CDROM_REG0 & ~3;
@@ -58,6 +60,7 @@ CESTER_TEST(raceGetLocPAndNop, test_instances,
 
     cester_assert_uint_eq(3, cause1);
     cester_assert_uint_eq(0xe0, cause1b);
+    cester_assert_uint_eq(0x98, ctrl0);
     cester_assert_uint_eq(0x38, ctrl1);
     cester_assert_uint_eq(0x18, ctrl2);
     cester_assert_uint_eq(2, response1[0]);
@@ -85,6 +88,8 @@ CESTER_TEST(raceNopAndGetLocP, test_instances,
     CDROM_REG0 = 0;
     CDROM_REG1 = CDL_NOP;
     CDROM_REG1 = CDL_GETLOCP;
+    uint8_t ctrl0 = CDROM_REG0 & ~3;
+
     uint32_t ackTime = waitCDRomIRQ();
     uint8_t cause1 = ackCDRomCause();
     uint8_t ctrl1 = CDROM_REG0 & ~3;
@@ -100,6 +105,7 @@ CESTER_TEST(raceNopAndGetLocP, test_instances,
 
     cester_assert_uint_eq(3, cause1);
     cester_assert_uint_eq(0xe0, cause1b);
+    cester_assert_uint_eq(0x98, ctrl0);
     cester_assert_uint_eq(0x38, ctrl1);
     cester_assert_uint_eq(0x18, ctrl2);
     cester_assert_uint_eq(5, response1[0]);
@@ -128,6 +134,7 @@ CESTER_TEST(raceNopAndGetTD1, test_instance,
     CDROM_REG2 = 1;
     CDROM_REG1 = CDL_NOP;
     CDROM_REG1 = CDL_GETTD;
+    uint8_t ctrl0 = CDROM_REG0 & ~3;
 
     uint32_t ackTime = waitCDRomIRQ();
     uint8_t cause1 = ackCDRomCause();
@@ -148,16 +155,16 @@ CESTER_TEST(raceNopAndGetTD1, test_instance,
     cester_assert_uint_eq(0, response1[1]);
     cester_assert_uint_eq(2, response1[2]);
     cester_assert_uint_eq(3, responseSize1);
+    cester_assert_uint_eq(0x90, ctrl0);
     cester_assert_uint_eq(0x38, ctrl1);
     cester_assert_uint_eq(0x18, ctrl2);
     cester_assert_false(gotIRQ);
     cester_assert_uint_ge(ackTime, 500);
     cester_assert_uint_lt(ackTime, 7000);
-    ramsyscall_printf("Basic getTD 1: ack in %ius\n", ackTime);
+    ramsyscall_printf("Nop followed by GetTD1: ack in %ius\n", ackTime);
 
     IMASK = imask;
 )
-
 
 CESTER_TEST(raceGetTD1AndNop, test_instance,
     uint32_t imask = IMASK;
@@ -176,6 +183,7 @@ CESTER_TEST(raceGetTD1AndNop, test_instance,
     CDROM_REG2 = 1;
     CDROM_REG1 = CDL_GETTD;
     CDROM_REG1 = CDL_NOP;
+    uint8_t ctrl0 = CDROM_REG0 & ~3;
 
     uint32_t ackTime = waitCDRomIRQ();
     uint8_t cause1 = ackCDRomCause();
@@ -195,12 +203,108 @@ CESTER_TEST(raceGetTD1AndNop, test_instance,
     cester_assert_uint_eq(3, response1[0]);
     cester_assert_uint_eq(0x20, response1[1]);
     cester_assert_uint_eq(2, responseSize1);
+    cester_assert_uint_eq(0x90, ctrl0);
     cester_assert_uint_eq(0x38, ctrl1);
     cester_assert_uint_eq(0x18, ctrl2);
     cester_assert_false(gotIRQ);
     cester_assert_uint_ge(ackTime, 500);
     cester_assert_uint_lt(ackTime, 7000);
-    ramsyscall_printf("Basic getTD 1: ack in %ius\n", ackTime);
+    ramsyscall_printf("GetTD1 followed by Nop: ack in %ius\n", ackTime);
+
+    IMASK = imask;
+)
+
+CESTER_TEST(cdlSeekP2to80, test_instance,
+    uint32_t imask = IMASK;
+
+    IMASK = imask | IRQ_CDROM;
+
+    int resetDone = resetCDRom();
+    if (!resetDone) {
+        cester_assert_true(resetDone);
+        return;
+    }
+
+    int seekDone = seekPTo(0, 2, 0);
+    if (!seekDone) {
+        cester_assert_true(seekDone);
+        return;
+    }
+
+    int setLocDone = setLoc(0x80, 0, 0);
+    if (!setLocDone) {
+        cester_assert_true(setLocDone);
+        return;
+    }
+
+    initializeTime();
+    // wait 50ms for things to settle
+    while (updateTime() < 50000);
+    initializeTime();
+
+    CDROM_REG0 = 0;
+    CDROM_REG1 = CDL_SEEKP;
+    uint8_t ctrl0 = CDROM_REG0 & ~3;
+
+    uint32_t ackTime = waitCDRomIRQ();
+    uint8_t cause1 = ackCDRomCause();
+    uint8_t ctrl1 = CDROM_REG0 & ~3;
+    uint8_t response1[16];
+    uint8_t responseSize1 = readResponse(response1);
+    uint8_t ctrl2 = CDROM_REG0 & ~3;
+    CDROM_REG0 = 1;
+    uint8_t cause1b = CDROM_REG3_UC;
+
+    CDROM_REG0 = 0;
+    CDROM_REG1 = CDL_NOP;
+    uint8_t ctrl2b = CDROM_REG0 & ~3;
+
+    initializeTime();
+    uint32_t ackTime2 = waitCDRomIRQ();
+    uint8_t cause2 = ackCDRomCause();
+    uint8_t ctrl3 = CDROM_REG0 & ~3;
+    uint8_t response2[16];
+    uint8_t responseSize2 = readResponse(response2);
+    uint8_t ctrl4 = CDROM_REG0 & ~3;
+    CDROM_REG0 = 1;
+    uint8_t cause2b = CDROM_REG3_UC;
+
+    initializeTime();
+    uint32_t timeout = 20000000;
+    int gotIRQ = waitCDRomIRQWithTimeout(&timeout);
+    uint8_t cause3 = ackCDRomCause();
+    uint8_t ctrl5 = CDROM_REG0 & ~3;
+    uint8_t response3[16];
+    uint8_t responseSize3 = readResponse(response3);
+    uint8_t ctrl6 = CDROM_REG0 & ~3;
+    CDROM_REG0 = 1;
+    uint8_t cause3b = CDROM_REG3_UC;
+
+    cester_assert_uint_eq(3, cause1);
+    cester_assert_uint_eq(3, cause2);
+    cester_assert_uint_eq(0xe0, cause1b);
+    cester_assert_uint_eq(0xe0, cause2b);
+    cester_assert_uint_eq(2, response1[0]);
+    cester_assert_uint_eq(1, responseSize1);
+    cester_assert_uint_eq(2, response2[0]);
+    cester_assert_uint_eq(1, responseSize2);
+    cester_assert_uint_eq(6, response3[0]);
+    cester_assert_uint_eq(0x10, response3[1]);
+    cester_assert_uint_eq(2, responseSize3);
+    cester_assert_uint_eq(0x98, ctrl0);
+    cester_assert_uint_eq(0x38, ctrl1);
+    cester_assert_uint_eq(0x18, ctrl2);
+    cester_assert_uint_eq(0x98, ctrl2b);
+    cester_assert_uint_eq(0x38, ctrl3);
+    cester_assert_uint_eq(0x18, ctrl4);
+    cester_assert_uint_eq(0x38, ctrl5);
+    cester_assert_uint_eq(0x18, ctrl6);
+    cester_assert_true(gotIRQ);
+    cester_assert_uint_ge(ackTime, 500);
+    cester_assert_uint_lt(ackTime, 7000);
+    cester_assert_uint_ge(ackTime2, 500);
+    cester_assert_uint_lt(ackTime2, 7000);
+    ramsyscall_printf("SeekP from 00:02:00 to 80:00:00 with Nop in between: ack in %ius, ack2 in %ius, errored in %ius\n", ackTime, ackTime2, timeout);
 
     IMASK = imask;
 )
