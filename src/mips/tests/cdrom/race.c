2b6f1619c98fc2c5c5f4a6bddece5d87db109c68
Nicolas Pixel Noble <pixel@nobis-crew.org>
2022-12-30 17:21:27 -0800

    One more race.

--- src/mips/tests/cdrom/race.c
+++ src/mips/tests/cdrom/race.c
@@ -270,8 +270,7 @@ CESTER_TEST(raceSeekP2to80WaitAckAndNop, test_instance,
     uint8_t cause2b = CDROM_REG3_UC;
 
     initializeTime();
-    uint32_t timeout = 20000000;
-    int gotIRQ = waitCDRomIRQWithTimeout(&timeout);
+    uint32_t errorTime = waitCDRomIRQ();
     uint8_t cause3 = ackCDRomCause();
     uint8_t ctrl5 = CDROM_REG0 & ~3;
     uint8_t response3[16];
@@ -299,12 +298,67 @@ CESTER_TEST(raceSeekP2to80WaitAckAndNop, test_instance,
     cester_assert_uint_eq(0x18, ctrl4);
     cester_assert_uint_eq(0x38, ctrl5);
     cester_assert_uint_eq(0x18, ctrl6);
-    cester_assert_true(gotIRQ);
     cester_assert_uint_ge(ackTime, 500);
     cester_assert_uint_lt(ackTime, 7000);
     cester_assert_uint_ge(ackTime2, 500);
     cester_assert_uint_lt(ackTime2, 7000);
-    ramsyscall_printf("SeekP from 00:02:00 to 80:00:00 with Nop in between: ack in %ius, ack2 in %ius, errored in %ius\n", ackTime, ackTime2, timeout);
+    ramsyscall_printf("SeekP from 00:02:00 to 80:00:00 with Nop in between: ack in %ius, ack2 in %ius, errored in %ius\n", ackTime, ackTime2, errorTime);
 
     IMASK = imask;
+)
+
+CESTER_TEST(raceNopWaitAndNop, test_instances,
+    int resetDone = resetCDRom();
+    if (!resetDone) {
+        cester_assert_true(resetDone);
+        return;
+    }
+
+    initializeTime();
+    CDROM_REG0 = 0;
+    CDROM_REG1 = CDL_NOP;
+    uint8_t ctrl0 = CDROM_REG0 & ~3;
+
+    while (updateTime() < 50000);
+    uint8_t ctrl0b = CDROM_REG0 & ~3;
+    CDROM_REG1 = CDL_NOP;
+
+    initializeTime();
+    uint32_t ackTime = waitCDRomIRQ();
+    uint8_t cause1 = ackCDRomCause();
+    uint8_t ctrl1 = CDROM_REG0 & ~3;
+    uint8_t response1[16];
+    uint8_t responseSize1 = readResponse(response1);
+    uint8_t ctrl2 = CDROM_REG0 & ~3;
+    CDROM_REG0 = 1;
+    uint8_t cause1b = CDROM_REG3_UC;
+
+    initializeTime();
+    uint32_t ackTime2 = waitCDRomIRQ();
+    uint8_t cause2 = ackCDRomCause();
+    uint8_t ctrl3 = CDROM_REG0 & ~3;
+    uint8_t response2[16];
+    uint8_t responseSize2 = readResponse(response2);
+    uint8_t ctrl4 = CDROM_REG0 & ~3;
+    CDROM_REG0 = 1;
+    uint8_t cause2b = CDROM_REG3_UC;
+
+    cester_assert_uint_eq(3, cause1);
+    cester_assert_uint_eq(3, cause2);
+    cester_assert_uint_eq(0xe0, cause1b);
+    cester_assert_uint_eq(0x98, ctrl0);
+    cester_assert_uint_eq(0x38, ctrl0b);
+    cester_assert_uint_eq(0xb8, ctrl1);
+    cester_assert_uint_eq(0x98, ctrl2);
+    cester_assert_uint_eq(0x38, ctrl3);
+    cester_assert_uint_eq(0x18, ctrl4);
+    cester_assert_uint_eq(2, response1[0]);
+    cester_assert_uint_eq(1, responseSize1);
+    cester_assert_uint_eq(2, response2[0]);
+    cester_assert_uint_eq(1, responseSize2);
+    // This one really ought to be 0, but let's give some slack anyway.
+    cester_assert_uint_lt(ackTime, 150);
+    cester_assert_uint_ge(ackTime2, 500);
+    cester_assert_uint_lt(ackTime2, 7000);
+    ramsyscall_printf("Nop followed by Nop, ack in %ius, ack2 in %ius\n", ackTime, ackTime2);
 )
