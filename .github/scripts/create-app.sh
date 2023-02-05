8f0f41291dc742f3cbfb0ae9407231e8f1163570
Thankful Machine <thankfulmachine@proton.me>
2023-02-04 22:19:39 -0800

    Improve mac app layout
    When downloading an app or disk image from the Web, files get the
    `com.apple.quarantine` attribute which signals to the OS that the app may be
    unsafe. The user can still let the app to run from Finder, which removes the
    attribute and allows future runs to proceed without a prompt.
    
    However, if an app is irregular, the user can't approve it normally and it may
    get labeled as "damaged". The workaround is to remove the attribute with
    something like `xattr` in the terminal but that isn't necessary.
    
    Change the `create-app.sh` script to bring the app into a more typical form for
    macOS and remove files that might interfere with code-signing or needlessly
    increase the app's size.
    
    - Don't symlink the `pcsx-redux` binary. Make the real binary exist at
      `./Contents/MacOS/PCSX-Redux`.
      - Remove the now empty `./Contents/Resources/bin`.
    - Call the app icon `AppIcon.icns` as is customary.
      - Remove source images after constructing the app icon.
    - Move dylibs into `./Contents/Frameworks`.
      - Add this path to the executable's `@rpath`.
      - Use `@rpath` instead of `@executable_path` for library locations.
    - Removing the quarantine attribute during the build has no effect.
    - Remove the Linux desktop things from
      `./Contents/Resources/share/applications` as it isn't necessary for macOS.
    - Ad-hoc codesign the app after making changes.
      This must *always* be the very last step.
    
    Update the README about this.

--- .github/scripts/create-app.sh
+++ .github/scripts/create-app.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-brew install imagemagick dylibbundler
-
 APP=PCSX-Redux
+APPROOT="${APP}.app"
+
+# /usr/libexec needed for PlistBuddy used below.
 PATH="$PATH:/usr/libexec"
 
+# ImageMagik used for converting .ico files to .png files.
+# dylibbundler used for updating load commands for dylib dependencies.
+brew install imagemagick dylibbundler
+
+# Construct the app iconset.
 mkdir pcsx-redux.iconset
 convert resources/pcsx-redux.ico[0] -alpha on -background none -units PixelsPerInch -density 72 -resize 16x16 pcsx-redux.iconset/icon_16x16.png
 convert resources/pcsx-redux.ico[0] -alpha on -background none -units PixelsPerInch -density 144 -resize 32x32 pcsx-redux.iconset/icon_16x16@2x.png
@@ -18,13 +24,40 @@ convert resources/pcsx-redux.ico[0] -alpha on -background none -units PixelsPerI
 convert resources/pcsx-redux.ico[0] -alpha on -background none -units PixelsPerInch -density 144 -resize 1024x1024 pcsx-redux.iconset/icon_512x512@2x.png
 iconutil --convert icns pcsx-redux.iconset
 
-make install DESTDIR=${APP}.app/Contents/Resources
-mkdir -p ${APP}.app/Contents/MacOS
-ln -s ../Resources/bin/pcsx-redux ${APP}.app/Contents/MacOS/${APP}
-PlistBuddy ${APP}.app/Contents/Info.plist -c "add CFBundleDisplayName string ${APP}"
-PlistBuddy ${APP}.app/Contents/Info.plist -c "add CFBundleIconFile string pcsx-redux.icns"
-PlistBuddy ${APP}.app/Contents/Info.plist -c "add NSHighResolutionCapable bool true"
-PlistBuddy ${APP}.app/Contents/version.plist -c "add ProjectName string ${APP}"
-dylibbundler -od -b -x ./PCSX-Redux.app/Contents/Resources/bin/pcsx-redux -d ./PCSX-Redux.app/Contents/Resources/lib/ -p @executable_path/../lib/
-cp pcsx-redux.icns ${APP}.app/Contents/Resources/
-xattr -r -d com.apple.quarantine ${APP}.app
+# Install the contents into ./Contents/Resources temporarily.
+make install DESTDIR=${APPROOT}/Contents/Resources
+
+# Move the executable to ./Contents/MacOS/PCSX-Redux.
+mkdir -p ${APPROOT}/Contents/MacOS
+mv ${APPROOT}/Contents/Resources/bin/pcsx-redux ${APPROOT}/Contents/MacOS/${APP}
+
+# Delete the now empty bin directory.
+rmdir ${APPROOT}/Contents/Resources/bin
+
+# Copy the app icon to the expected location.
+cp pcsx-redux.icns ${APPROOT}/Contents/Resources/AppIcon.icns
+
+# Remove source images that were used to create the app icon.
+rm -rfv ${APPROOT}/Contents/Resources/share/icons
+
+# Create the required Info.plist and version.plist files
+# with the minimum information.
+PlistBuddy ${APPROOT}/Contents/Info.plist -c "add CFBundleDisplayName string ${APP}"
+PlistBuddy ${APPROOT}/Contents/Info.plist -c "add CFBundleIconName string AppIcon"
+PlistBuddy ${APPROOT}/Contents/Info.plist -c "add CFBundleIconFile string AppIcon"
+PlistBuddy ${APPROOT}/Contents/Info.plist -c "add NSHighResolutionCapable bool true"
+PlistBuddy ${APPROOT}/Contents/version.plist -c "add ProjectName string ${APP}"
+
+# Install dylib dependencies in ./Contents/Frameworks.
+# Update the dyld load commands for these.
+dylibbundler -od -b -x ${APPROOT}/Contents/MacOS/${APP} -d ${APPROOT}/Contents/Frameworks/ -p @rpath
+
+# Add a relative @rpath to ./Contents/Frameworks
+# so that dyld knows where to find dylib dependencies.
+install_name_tool -add_rpath @loader_path/../Frameworks ${APPROOT}/Contents/MacOS/${APP}
+
+# Linux desktop shortcuts not relevant.
+rm -rfv ${APPROOT}/Contents/Resources/share/applications
+
+# Ad-hoc codesign the app.
+codesign --force -s - -vvvv ${APPROOT}
