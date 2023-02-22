78612dbbaa888cacf30e0a7d68ebc7beff6e2b11
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2023-02-21 21:04:08 -0800

    Hopefully fixing Windows.

--- tools/vscode-extension/terminal.js
+++ tools/vscode-extension/terminal.js
@@ -1,12 +1,13 @@
 'use strict'
 
 const vscode = require('vscode')
+const which = require('which')
 
-exports.run = function (binary, args) {
+exports.run = async function (binary, args) {
   if (process.platform === 'win32') {
     args = args.join(' ')
   }
-  const terminal = vscode.window.createTerminal('PSX.Dev', binary, args)
+  const terminal = vscode.window.createTerminal('PSX.Dev', await which(binary), args)
   terminal.show()
   let resolver
   let rejecter
