d412bbf9203c6041f00eca5ea11cd0d89d2ff07d
Nicolas Pixel Noble <pixel@nobis-crew.org>
2023-08-31 21:43:16 -0700

    Upgrading to 640x480, and using PolyLines.

--- src/mips/psyqo/examples/bezier/bezier.cpp
+++ src/mips/psyqo/examples/bezier/bezier.cpp
@@ -33,6 +33,7 @@ SOFTWARE.
 #include "psyqo/scene.hh"
 #include "psyqo/trigonometry.hh"
 
+using namespace psyqo::fixed_point_literals;
 using namespace psyqo::trig_literals;
 
 namespace {
@@ -48,11 +49,18 @@ class Bezier final : public psyqo::Application {
 
 class BezierScene final : public psyqo::Scene {
     void frame() override;
-    psyqo::Prim::Line m_line{{.r = 0xff, .g = 0x80, .b = 0x33}};
+    psyqo::Prim::PolyLine<20> m_lines{{.r = 0xff, .g = 0x80, .b = 0x33}};
     psyqo::Angle m_angle1 = 0.0_pi;
     psyqo::Angle m_angle2 = 0.0_pi;
-    constexpr static psyqo::Angle m_angleStep1 = 0.0135_pi;
-    constexpr static psyqo::Angle m_angleStep2 = 0.017_pi;
+    constexpr static psyqo::Angle c_angleStep1 = 0.0135_pi;
+    constexpr static psyqo::Angle c_angleStep2 = 0.017_pi;
+    constexpr static psyqo::Vec2 a = {0.0_fp, 240.0_fp};
+    constexpr static psyqo::Vec2 b = {640.0_fp, 240.0_fp};
+  public:
+    BezierScene() {
+        m_lines.points[0] = a;
+        m_lines.points[20] = b;
+    }
 };
 
 // We're instantiating the two objects above right now.
@@ -63,10 +71,10 @@ BezierScene bezierScene;
 
 void Bezier::prepare() {
     psyqo::GPU::Configuration config;
-    config.set(psyqo::GPU::Resolution::W320)
+    config.set(psyqo::GPU::Resolution::W640)
         .set(psyqo::GPU::VideoMode::AUTO)
         .set(psyqo::GPU::ColorMode::C15BITS)
-        .set(psyqo::GPU::Interlace::PROGRESSIVE);
+        .set(psyqo::GPU::Interlace::INTERLACED);
     gpu().initialize(config);
 }
 
@@ -76,28 +84,41 @@ void Bezier::createScene() {
 }
 
 void BezierScene::frame() {
-    using namespace psyqo::fixed_point_literals;
-    const psyqo::Vec2 a = {0.0_fp, 100.0_fp};
-    const psyqo::Vec2 b = {320.0_fp, 100.0_fp};
-    psyqo::Vec2 m = a;
-    m_angle1 += m_angleStep1;
-    m_angle2 += m_angleStep2;
-    psyqo::FixedPoint<> p1 = bezier.m_trig.sin(m_angle1) * 100.0_fp + 100.0_fp;
-    psyqo::FixedPoint<> p2 = bezier.m_trig.sin(m_angle2) * 100.0_fp + 100.0_fp;
+    m_angle1 += c_angleStep1;
+    m_angle2 += c_angleStep2;
+    if (m_angle1 > 2.0_pi) {
+        m_angle1 -= 2.0_pi;
+    }
+    if (m_angle2 > 2.0_pi) {
+        m_angle2 -= 2.0_pi;
+    }
+    auto angle1 = psyqo::Angle(bezier.m_trig.sin(m_angle1)) * 0.5_pi;
+    auto angle2 = psyqo::Angle(bezier.m_trig.sin(m_angle2)) * 0.5_pi;
+    auto p1x = bezier.m_trig.cos(angle1) * 200.0_fp;
+    if (p1x < 0) p1x = -p1x;
+    auto p1y = bezier.m_trig.sin(angle1) * 200.0_fp + 240.0_fp;
+    auto p2x = bezier.m_trig.cos(angle2) * 200.0_fp;
+    if (p2x < 0) p2x = -p2x;
+    auto p2y = bezier.m_trig.sin(angle2) * 200.0_fp + 240.0_fp;
+    psyqo::Vec2 p1({p1x, p1y}), p2({640.0_fp - p2x, p2y});
     // Clear the screen and draw the Bezier lines
     gpu().clear({{.r = 0x68, .g = 0xb0, .b = 0xd8}});
-    psyqo::FixedPoint<> t = 0.05_fp;
-    for (int i = 1; i < 20; ++i) {
+    psyqo::FixedPoint<> t;
+    for (int i = 1; i < 20; i++) {
         t += 0.05_fp;
-        const psyqo::Vec2 n = psyqo::Bezier::cubic(a, {0.0_fp, p1}, {320.0_fp, p2}, b, t);
-        m_line.pointA.x = m.x.integer();
-        m_line.pointA.y = m.y.integer();
-        m_line.pointB.x = n.x.integer();
-        m_line.pointB.y = n.y.integer();
-        gpu().sendPrimitive(m_line);
-        m = n;
+        m_lines.points[i] = psyqo::Bezier::cubic(a, p1, p2, b, t);
     }
-    gpu().sendPrimitive(m_line);
+    gpu().sendPrimitive(m_lines);
+
+#ifdef DEBUG_BEZIER
+    psyqo::Prim::Line line;
+    line.pointA = a;
+    line.pointB = p1;
+    gpu().sendPrimitive(line);
+    line.pointA = p2;
+    line.pointB = b;
+    gpu().sendPrimitive(line);
+#endif
 }
 
 int main() { return bezier.run(); }
