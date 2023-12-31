2a80e502927af936a8a14780512cf0074402ef90
Nicolas Pixel Noble <pixel@nobis-crew.org>
2023-12-31 10:53:35 -0800

    'tis that time of the year again.

--- src/core/r3000a.cc
+++ src/core/r3000a.cc
@@ -37,7 +37,7 @@
 
 int PCSX::R3000Acpu::psxInit() {
     g_system->printf(_("PCSX-Redux booting\n"));
-    g_system->printf(_("Copyright (C) 2019-2023 PCSX-Redux authors\n"));
+    g_system->printf(_("Copyright (C) 2019-2024 PCSX-Redux authors\n"));
     const auto& args = g_system->getArgs();
 
     if (g_emulator->settings.get<Emulator::SettingDynarec>()) {
