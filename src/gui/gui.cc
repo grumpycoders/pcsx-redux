18a326e26aea32eadade5296daea123b50189021
Ken Murdock <ken.murdock@giragast.com>
2024-04-24 02:34:10 +0100

    Allow keyboard modifiers to adjust the target Memory Editor in the Assembly widget Add context menu options to open an address in any Memory Editor

--- src/gui/gui.cc
+++ src/gui/gui.cc
@@ -740,6 +740,8 @@ void PCSX::GUI::init(std::function<void()> applyArguments) {
         const uint32_t base = (event.address >> 20) & 0xff8;
         const uint32_t real = event.address & 0x7fffff;
         const uint32_t size = event.size;
+        const uint32_t editorNum = event.editorNum;
+        const bool forceShowEditor = event.forceShowEditor;
         auto changeDataType = [](MemoryEditor* editor, int size) {
             bool isSigned = false;
             switch (editor->PreviewDataType) {
@@ -764,11 +766,13 @@ void PCSX::GUI::init(std::function<void()> applyArguments) {
         };
         if ((base == 0x000) || (base == 0x800) || (base == 0xa00)) {
             if (real < 0x00800000) {
-                m_mainMemEditors[0].editor.GotoAddrAndHighlight(real, real + size);
-                changeDataType(&m_mainMemEditors[0].editor, size);
+                if (forceShowEditor) m_mainMemEditors[editorNum].m_show = true;
+                m_mainMemEditors[editorNum].editor.GotoAddrAndHighlight(real, real + size);
+                changeDataType(&m_mainMemEditors[editorNum].editor, size);
             }
         } else if (base == 0x1f8) {
             if (real >= 0x1000 && real < 0x3000) {
+                if (forceShowEditor) m_hwrEditor.m_show = true;
                 m_hwrEditor.editor.GotoAddrAndHighlight(real - 0x1000, real - 0x1000 + size);
                 changeDataType(&m_hwrEditor.editor, size);
             }
