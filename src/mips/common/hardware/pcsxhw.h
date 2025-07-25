e1be09367b24cbe826d386c613d992235bb06784
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2025-07-24 17:43:46 -0700

    Fixing accesses to the pcsx IO system.

--- src/mips/common/hardware/pcsxhw.h
+++ src/mips/common/hardware/pcsxhw.h
@@ -39,22 +39,28 @@ static __inline__ void pcsx_initMsan() { *((volatile char* const)0x1f802089) = 0
 static __inline__ void pcsx_resetMsan() { *((volatile char* const)0x1f802089) = 1; }
 static __inline__ void* pcsx_msanAlloc(uint32_t size) {
     register uint32_t a0 asm("a0") = size;
-    return *((void* volatile* const)0x1f80208c);
+    void* ret;
+    __asm__ volatile("lw %0, 0x208c(%1)" : "=r"(ret) : "r"(0x1f800000), "r"(a0));
+    return ret;
 }
 static __inline__ void pcsx_msanFree(void* ptr) { *((void* volatile* const)0x1f80208c) = ptr; }
 static __inline__ void* pcsx_msanRealloc(void* ptr, uint32_t size) {
     register void* a0 asm("a0") = ptr;
     register uint32_t a1 asm("a1") = size;
-    return *((void* volatile* const)0x1f802090);
+    void* ret;
+    __asm__ volatile("lw %0, 0x2090(%1)" : "=r"(ret) : "r"(0x1f800000), "r"(a0), "r"(a1));
+    return ret;
 }
 static __inline__ void pcsx_msanSetChainPtr(void* headerAddr, void* ptrToNext, uint32_t wordCount) {
     register void* a0 asm("a0") = ptrToNext;
     register uint32_t a1 asm("a1") = wordCount;
-    *((void* volatile* const)0x1f802094) = headerAddr;
+    __asm__ volatile("sw %0, 0x2094(%1)" : : "r"(a0), "r"(0x1f800000), "r"(a1));
 }
 static __inline__ void* pcsx_msanGetChainPtr(void* headerAddr) {
     register void* a0 asm("a0") = headerAddr;
-    return *((void* volatile* const)0x1f802094);
+    void* ret;
+    __asm__ volatile("lw %0, 0x2094(%1)" : "=r"(ret) : "r"(0x1f800000), "r"(a0));
+    return ret;
 }
 
 static __inline__ int pcsx_present() { return *((volatile uint32_t* const)0x1f802080) == 0x58534350; }
