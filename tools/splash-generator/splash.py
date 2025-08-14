0b73a07a752c71db23e30ea338b7bb42069c05a9
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2025-08-14 14:19:41 +0300

    Add splash screen generation script

---
+++ tools/splash-generator/splash.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Generates the splash screen data the emulator displays while nothing is loaded
+import sys
+from PIL import Image
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <image_file>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+img = Image.open(filename).convert("RGBA")
+width, height = img.size
+pixels = list(img.getdata())
+
+# Convert pixel data to RGBA8888
+data = []
+for r, g, b, a in pixels:
+    val = (a << 24) | (b << 16) | (g << 8) | r
+    data.append(val)
+
+print("// Generated using tools/splash-generator/splash.py")
+# First 2 u32s are the splash screen's width and height
+# The rest of the array is the pixel data in little endian RGBA8888
+print(f"static const uint32_t s_splashImageData[] = {{")
+print(f"    {width}, {height},", end=" ")
+
+for i, val in enumerate(data):
+    sep = "," if i != len(data) - 1 else ""
+    if i % 8 == 0:
+        print("\n    ", end="")
+    print(f"0x{val:08X}{sep}", end=" ")
+print("\n};")
