c319254ebcd744fb0d85afb80f50ceb0d3b0d539
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2025-08-31 23:19:16 -0700

    Adding zlib patch for binutils on macos...

--- tools/vscode-extension/scripts/mipsel-none-elf-binutils.rb
+++ tools/vscode-extension/scripts/mipsel-none-elf-binutils.rb
@@ -7,6 +7,8 @@ class MipselNoneElfBinutils < Formula
 
   depends_on "texinfo" => :build
 
+  patch :DATA
+
   def install
     system "./configure", "--target=mipsel-none-elf",
                           "--disable-multilib",
@@ -20,3 +22,47 @@ class MipselNoneElfBinutils < Formula
     assert_match "f()", shell_output("#{bin}/mipsel-none-elf-c++filt _Z1fv")
   end
 end
+
+__END__
+diff --git a/zlib/zutil.h b/zlib/zutil.h
+index 0bd2dbcba..bb513cb4b 100644
+--- a/zlib/zutil.h
++++ b/zlib/zutil.h
+@@ -137,17 +137,8 @@ extern z_const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
+ #  endif
+ #endif
+
+-#if defined(MACOS) || defined(TARGET_OS_MAC)
++#if defined(MACOS)
+ #  define OS_CODE  7
+-#  ifndef Z_SOLO
+-#    if defined(__MWERKS__) && __dest_os != __be_os && __dest_os != __win32_os
+-#      include <unix.h> /* for fdopen */
+-#    else
+-#      ifndef fdopen
+-#        define fdopen(fd,mode) NULL /* No fdopen() */
+-#      endif
+-#    endif
+-#  endif
+ #endif
+
+ #ifdef __acorn
+@@ -170,18 +161,6 @@ extern z_const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
+ #  define OS_CODE 19
+ #endif
+
+-#if defined(_BEOS_) || defined(RISCOS)
+-#  define fdopen(fd,mode) NULL /* No fdopen() */
+-#endif
+-
+-#if (defined(_MSC_VER) && (_MSC_VER > 600)) && !defined __INTERIX
+-#  if defined(_WIN32_WCE)
+-#    define fdopen(fd,mode) NULL /* No fdopen() */
+-#  else
+-#    define fdopen(fd,type)  _fdopen(fd,type)
+-#  endif
+-#endif
+-
+ #if defined(__BORLANDC__) && !defined(MSDOS)
+   #pragma warn -8004
+   #pragma warn -8008
