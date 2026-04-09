1763832c8390d08de847a52a072e297e5bee1ccf
John Baumann <johnbaumann@live.com>
2026-04-08 21:08:49 -0500

    derpderpderp

--- src/core/pcsxffi.lua
+++ src/core/pcsxffi.lua
@@ -175,7 +175,7 @@ end
 
 local function scheduleInterrupt(eCycle)
     if type(eCycle) ~= 'number' then error 'PCSX.scheduleInterrupt requires a numeric cycle count' end
-    if (eCycle < 0) or (eCycle > 0xffffffff) or (eCycle % 1 ~= 0) then error 'PCSX.scheduleInterrupt cycle count parameter should be an unsigned 32-bit integer'
+    if (eCycle < 0) or (eCycle > 0xffffffff) or (eCycle % 1 ~= 0) then error 'PCSX.scheduleInterrupt cycle count parameter should be an unsigned 32-bit integer' end
     C.scheduleInterrupt(eCycle)
 end
 
