68b34dfeb2ed613a1b82e92e30e78a09ddf61b31
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2026-08-27 14:21:45 -0700

    Build the mips toolchain from source in the build image.
    Ubuntu 26.04 has no mips cross compiler to install, so a first stage builds
    binutils and gcc with spawn-compiler.sh and the final image copies the result
    out of it. Scratch branch: this exists only so the image can be built from the
    SDL3 recipe before the migration lands, and it is not meant to be merged.

--- tools/linux-mips/spawn-compiler.sh
+++ tools/linux-mips/spawn-compiler.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 #
 # Building gcc from source isn't necessarily an easy task, and it requires
-# a lot of various dependency packages to work properly. The easiest way
-# to use a mips compiler for Linux is still to use a package from your
-# distribution. Nevertheless, here's how to build a working mips compiler
-# that can produce code for the R3000A CPU, granted you have the necessary
-# dependencies to do so. The script can be run as root, or as a regular user
-# when the PREFIX environment variable is provided.
+# a lot of various dependency packages to work properly. Here's how to build
+# a working mips compiler that can produce code for the R3000A CPU, granted
+# you have the necessary dependencies to do so. The script can be run as
+# root, or as a regular user when the PREFIX environment variable is
+# provided. Set JOBS to override the parallelism, which defaults to the
+# number of available cores.
 #
 
 set -ex
 
 PREFIX=${PREFIX:-"/usr/local"}
+JOBS=${JOBS:-$(nproc 2> /dev/null || echo 1)}
 
 for url in https://ftpmirror.gnu.org/gnu/binutils/binutils-2.47.tar.gz https://mirrors.kernel.org/gnu/binutils/binutils-2.47.tar.gz ; do
     wget --max-redirect=2 --timeout=60 --continue --trust-server-names $url && break
@@ -19,7 +20,7 @@ done
 tar xvfz binutils-2.47.tar.gz
 cd binutils-2.47
 ./configure --target=mipsel-none-elf --disable-multilib --disable-nls --disable-werror --prefix=$PREFIX
-make
+make -j$JOBS
 make install-strip
 cd ..
 
@@ -32,9 +33,9 @@ cd gcc-16.2.0
 mkdir build
 cd build
 ../configure --target=mipsel-none-elf --without-isl --disable-nls --disable-threads --disable-shared --disable-libssp --disable-libstdcxx-pch --disable-libgomp --disable-werror --without-headers --disable-hosted-libstdcxx --with-as=$PREFIX/bin/mipsel-none-elf-as --with-ld=$PREFIX/bin/mipsel-none-elf-ld --enable-languages=c,c++ --prefix=$PREFIX
-make all-gcc
+make -j$JOBS all-gcc
 make install-strip-gcc
-make all-target-libgcc
+make -j$JOBS all-target-libgcc
 make install-strip-target-libgcc
-make all-target-libstdc++-v3
+make -j$JOBS all-target-libstdc++-v3
 make install-strip-target-libstdc++-v3
